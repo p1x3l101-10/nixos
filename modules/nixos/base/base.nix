8c1b874de771d3de027087f90da80b9273ed9925
Pixel <scott.blatt.0b10@gmail.com>
2025-02-05 14:53:54 -0600

    typo in path

--- modules/nixos/base/base.nix
+++ modules/nixos/base/base.nix
@@ -17,7 +17,7 @@
   boot.initrd.systemd.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.package = (pkgs.callPackage ../../packages/only-nix3 {});
+  nix.package = (pkgs.callPackage ../../../packages/only-nix3 {});
   programs.git = {
     enable = true;
     package = lib.mkDefault pkgs.gitMinimal;
