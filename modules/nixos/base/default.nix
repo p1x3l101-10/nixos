99541c3cbec7af25c5f62e56eee1248131857b16
Pixel <scott.blatt.0b10@gmail.com>
2025-02-05 10:39:38 -0600

    remove self ref

--- modules/nixos/base/default.nix
+++ modules/nixos/base/default.nix
@@ -1,7 +1,6 @@
 { inputs, ... }:
 {
   imports = with inputs; [
-    self.nixosModules.base
     lanzaboote.nixosModules.lanzaboote
     impermanence.nixosModules.impermanence
     disko.nixosModules.disko
