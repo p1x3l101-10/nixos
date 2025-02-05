3811845ae0df581dd5dd101d2795c1e56cd40340
Pixel <scott.blatt.0b10@gmail.com>
2025-02-05 13:34:33 -0600

    remove concatlines

--- overlays/only-nix3/default.nix
+++ overlays/only-nix3/default.nix
@@ -4,18 +4,16 @@ let
 in
 final: prev: {
   nix = prev.nix.overrideAttrs (oldAttrs: {
-    installPhase = lib.concatLines [
-      oldAttrs.installPhase
-      ''
-        # Remove nix2 symlinks
-        for bin in $out/bin/*; do
-          if [[ "$(basename "$bin")" != "nix" && -L "$bin" ]] ; then
-            rm -v "$bin"
-          fi
-        done
-        # Fix nix-daemon.service to use `nix daemon`
-        sed -i 's|^ExecStart=.*|ExecStart=@${prev.nix}/bin/nix daemon|' $out/lib/systemd/system/nix-daemon.service
-      ''
-    ];
+    installPhase = ''
+      ${oldAttrs.installPhase}
+      # Remove nix2 symlinks
+      for bin in $out/bin/*; do
+        if [[ "$(basename "$bin")" != "nix" && -L "$bin" ]] ; then
+          rm -v "$bin"
+        fi
+      done
+      # Fix nix-daemon.service to use `nix daemon`
+      sed -i 's|^ExecStart=.*|ExecStart=@${prev.nix}/bin/nix daemon|' $out/lib/systemd/system/nix-daemon.service
+    '';
   });
 }
