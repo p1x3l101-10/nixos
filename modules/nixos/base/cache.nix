a47b02ffc152fd70de36258721a150c55bffbcef
Pixel <scott.blatt.0b10@gmail.com>
2025-02-04 22:53:14 -0600

    attempt tmpfiles based cache clear

--- modules/nixos/base/cache.nix
+++ modules/nixos/base/cache.nix
@@ -10,13 +10,17 @@
         mountOptions = [ "defaults" "size=5G" "mode=755" ];
     };
     # Clear cache on reboot
+    /*
     boot.initrd.systemd.services.cleanSystemCache = {
         description = "Clean system disk cache";
         script = "${pkgs.coreutils}/bin/rm -rfv /sysroot/nix/host/cache";
         wantedBy = [ "initrd.target" ];
+        after = [ "initrd-root-fs.target" ];
         unitConfig = {
             RequiresMountsFor = "/sysroot/nix";
             Type = "Oneshot";
+            RemainAfterExit = true;
         };
-    };
+    };*/
+    boot.initrd.systemd.tmpfiles.settings."00-clean-cache"."/sysroot/nix/host/cache".R.argument = "0";
 }
