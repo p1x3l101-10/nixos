dc733296ca396dec3f52272186ba6aa3e02bd565
Pixel <scott.blatt.0b10@gmail.com>
2025-02-06 09:44:03 -0600

    darwin

--- modules/darwin/school/base.nix
+++ modules/darwin/school/base.nix
@@ -1,5 +1,11 @@
-{ ... }:
-
-{
+{ pkgs, ... }:
+let
+  pfx = "/private/var/tmp/nix";
+in {
+  nix.package = pkgs.nixVersions.latest.override {
+    storeDir = "${pfx}/store";
+    stateDir = "${pfx}/var";
+    confDir = "${pfx}/etc";
+  };
   system.stateVersion = 6;
 }
