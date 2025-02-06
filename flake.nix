231b495eaf6b30332854c2600be802fdfd37117c
Pixel <scott.blatt.0b10@gmail.com>
2025-02-05 22:06:02 -0600

    typo

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
 
     # Macbook things
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin-24.11";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-homebrew = {
