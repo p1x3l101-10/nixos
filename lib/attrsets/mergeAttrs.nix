51becd2e92de3cbaf03bc06dc28ab4598cfde6ac
Pixel <scott.blatt.0b10@gmail.com>
2025-02-08 19:21:26 -0600

    fix lib function

--- lib/attrsets/mergeAttrs.nix
+++ lib/attrsets/mergeAttrs.nix
@@ -4,12 +4,12 @@ attrList:
 
 let f = attrPath:
   lib.attrsets.zipAttrsWith (n: values:
-    if lib.builtins.tail values == []
-      then lib.builtins.head values
-    else if all lib.builtins.isList values
-      then lib.lists.unique (lib.builtins.concatLists values)
-    else if all lib.builtins.isAttrs values
+    if builtins.tail values == []
+      then builtins.head values
+    else if builtins.all builtins.isList values
+      then lib.lists.unique (builtins.concatLists values)
+    else if builtins.all builtins.isAttrs values
       then f (attrPath ++ [n]) values
-    else last values
+    else lib.lists.last values
   );
 in f [] attrList
