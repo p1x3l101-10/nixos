3e4b81feec2da81f0aaeb07889c0da9cf9de8da8
Pixel <scott.blatt.0b10@gmail.com>
2025-02-08 18:36:57 -0600

    make toString on forEach

--- lib/environment/mkEnvRawList.nix
+++ lib/environment/mkEnvRawList.nix
@@ -3,7 +3,7 @@
 name: value: seperator:
 
 if (value != []) then (
-  lib.internal.attrsets.mkEnvRaw (lib.strings.toUpper name) (lib.strings.concatStringsSep seperator (builtins.toString value))
+  lib.internal.attrsets.mkEnvRaw (lib.strings.toUpper name) (lib.strings.concatStringsSep seperator (lib.forEach value (x: builtins.toString x)))
 ) else (
   {}
 )
