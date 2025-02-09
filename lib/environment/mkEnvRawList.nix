cf9966b557cee278e79785b62d4b118d91cbd356
Pixel <scott.blatt.0b10@gmail.com>
2025-02-08 18:34:14 -0600

    make strings enforced

--- lib/environment/mkEnvRawList.nix
+++ lib/environment/mkEnvRawList.nix
@@ -3,7 +3,7 @@
 name: value: seperator:
 
 if (value != []) then (
-  lib.internal.attrsets.mkEnvRaw (lib.strings.toUpper name) (lib.strings.concatStringsSep seperator value)
+  lib.internal.attrsets.mkEnvRaw (lib.strings.toUpper name) (lib.strings.concatStringsSep seperator (builtins.toString value))
 ) else (
   {}
 )
