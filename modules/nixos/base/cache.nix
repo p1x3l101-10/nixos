f58440ff1e0834faeb07416fed9069db89591775
Pixel <scott.blatt.0b10@gmail.com>
2025-03-07 17:24:26 -0600

    split cache into 2 stage

--- modules/nixos/base/cache.nix
+++ modules/nixos/base/cache.nix
@@ -6,17 +6,29 @@
     "/tmp"
   ];
   # Clear cache on reboot
-  boot.initrd.systemd.services.cleanSystemCache = {
-    description = "Clean system disk cache";
+  boot.initrd.systemd.services.moveSystemCache = {
+    description = "Clean system disk cache (Stage 1)";
     script = ''
-      ${pkgs.coreutils}/bin/rm -rfv /sysroot/nix/host/cache
-      echo "Sucessfully cleared cache!"
-    ''; # NOTE: if you do not see the last line, bump the timeout number and try again
+      ${pkgs.coreutils}/bin/mv -v /sysroot/nix/host/cache /sysroot/nix/host/.cache.old
+      ${pkgs.coreutils}/bin/mkdir /sysroot/nix/host/cache
+      echo "Sucessfully cleaned cache!"
+    '';
     wantedBy = [ "initrd.target" ];
     after = [ "initrd-root-fs.target" ];
     unitConfig = {
       RequiresMountsFor = "/sysroot/nix";
-      Type = "Oneshot";
+      Type = "Oneshot"; # Ensure this task is completed before system boot
+    };
+  };
+  systemd.services.clearOldCache = {
+    description = "Clean system disk cache (Stage 2)";
+    script = ''
+      ${pkgs.coreutils}/bin/rm -rfv /sysroot/nix/host/.cache.old
+      echo "Sucessfully cleared old cache!"
+    ''; # NOTE: if you do not see the last line, bump the timeout number and try again
+    wantedBy = [ "multi-user.target" ];
+    unitConfig = {
+      RequiresMountsFor = "/nix";
       RemainAfterExit = true;
       TimeoutStartSec = "300"; # Ensure there is enough time for all directories to get removed
     };
