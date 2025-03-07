603434fea8dc6dbeccfe14aa8efefb5f33b331c6
Pixel <scott.blatt.0b10@gmail.com>
2025-03-07 17:46:20 -0600

    add drawing tablet stuff

--- systems/x86_64-linux/pixels-pc/hardware-configuration.nix
+++ systems/x86_64-linux/pixels-pc/hardware-configuration.nix
@@ -12,4 +12,9 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  services.xserver.digimend.enable = true;
+  environment.systemPackages = [
+    config.boot.kernelPackages.digimend
+  ];
 }
