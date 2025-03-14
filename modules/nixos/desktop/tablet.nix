de7dfd08218b77c429f8f256e08fa693ebb7e218
Pixel <scott.blatt.0b10@gmail.com>
2025-03-14 18:41:08 -0500

    fix udev rule

--- modules/nixos/desktop/tablet.nix
+++ modules/nixos/desktop/tablet.nix
@@ -3,6 +3,6 @@
 {
   users.groups.tablet = {};
   services.udev.extraRules = ''
-    'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="tablet"'
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="tablet"
   '';
 }
