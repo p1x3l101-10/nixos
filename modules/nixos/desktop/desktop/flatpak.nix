ff88fa8846e865cc9c65a281b79584bddd7a958e
Pixel <scott.blatt.0b10@gmail.com>
2025-03-14 17:42:57 -0500

    add subdirs to flatpak directory

--- modules/nixos/desktop/desktop/flatpak.nix
+++ modules/nixos/desktop/desktop/flatpak.nix
@@ -1,11 +1,30 @@
-{ ... }:
-{
+{ lib, ... }:
+let
+  mkTmp = { baseDir, permissions, subdirs ? []}: builtins.listToAttrs (
+    [ { name = baseDir; value = { d = permissions; }; } ] ++
+    lib.lists.forEach subdirs (x:
+      lib.attrsets.nameValuePair "${baseDir}/${x}" { d = permissions; }
+    )
+  );
+in {
   services.flatpak.enable = true;
   hardware.steam-hardware.enable = true;
   # Fix dir permissions
-  systemd.tmpfiles.settings."99-permission-fixes"."/var/lib/flatpak/repo".d = {
-    user = "root";
-    group = "root";
-    mode = "0777";
+  systemd.tmpfiles.settings."99-permission-fixes" = mkTmp {
+    baseDir = "/var/lib/flatpak";
+    permissions = {
+      user = "root";
+      group = "root";
+      mode = "0777";
+    };
+    subdirs = [
+      "app"
+      "appstream"
+      "db"
+      "exports"
+      "overrides"
+      "repo"
+      "runtime"
+    ];
   };
 }
