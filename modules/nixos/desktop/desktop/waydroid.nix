cdcb7011bbda112d0520a019a82aaa083ff1ddce
Pixel <scott.blatt.0b10@gmail.com>
2025-03-22 11:59:14 -0500

    waydroid settings

--- modules/nixos/desktop/desktop/waydroid.nix
+++ modules/nixos/desktop/desktop/waydroid.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   virtualisation.waydroid.enable = true;
+  # Tell waydroid to use memfd and not ashmem
+  systemd.tmpfiles.settings."99-waydroid-settings"."/var/lib/waydroid/waydroid_base.prop".C = {
+    user = "root";
+    group = "root";
+    mode = "0644";
+    argument = builtins.toString (pkgs.writeText "waydroid_base.prop" ''
+      sys.use_memfd=true
+    '');
+  };
 }
