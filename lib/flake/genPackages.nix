a523d992d4e0f5c33c2076a07c972d63174cefac
Pixel <scott.blatt.0b10@gmail.com>
2025-04-02 18:43:41 -0500

    calls

--- lib/flake/genPackages.nix
+++ lib/flake/genPackages.nix
@@ -1,13 +1,13 @@
 { lib, ext }:
 
-{ src, newScope, genModules ? lib.internal.flake.genModules }@args:
+src: newScope:
 
 lib.makeScope newScope (self:
   (
     lib.attrsets.mapAttrs (name: value:
       (self.callPackage value { })
     ) (
-      genModules { inherit src; }
+      lib.internal.flake.genModules { inherit src; }
     )
   )
 )
