f1f605462ceac55110877976420ad5565f443671
Pixel <scott.blatt.0b10@gmail.com>
2025-04-02 22:42:27 -0500

    fix min base

--- systems/do-droplet/base-min.nix
+++ systems/do-droplet/base-min.nix
@@ -5,9 +5,36 @@ let
 in
 {
   imports = [
-    "${base}/base.nix"
     "${base}/cachix.nix"
     "${base}/locale.nix"
     "${base}/ssh.nix"
   ];
+  networking = {
+    dhcpcd.enable = false;
+    networkmanager.enable = lib.mkForce false;
+    useDHCP = false;
+    useNetworkd = true;
+    usePredictableInterfaceNames = true;
+  };
+  systemd.network = {
+    enable = true;
+    wait-online.enable = true;
+    networks = {
+      "10-virtual-eth" = {
+        name = "eth0";
+        DHCP = "yes";
+        linkConfig.RequiredForOnline = "routable";
+      };
+    };
+  };
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    llmnr = "true";
+    dnsovertls = "true";
+  };
+  programs.nano.enable = lib.mkDefault false;
+  system.stateVersion = "24.11";
 }
