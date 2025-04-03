c6f95cbf03bd493528c050c09e81a654ec8430c3
Pixel <scott.blatt.0b10@gmail.com>
2025-04-02 22:19:12 -0500

    use userdata

--- modules/nixos/server/server-base/user.nix
+++ modules/nixos/server/server-base/user.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
+
+let
+  getdata = key: names: (import ../userdata.nix { inherit lib; }).getdata key names;
+in
 {
   users.users.pixel = {
     isNormalUser = true;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7hEnkX2r9nnIoVUa+isMwtdEppqWMTU9VDVE47ftLb MacBook"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9YD5mX4qfWS35Kcrk2hymaXlcSEUs3lWYa9bLKOcNW Pixels-PC"
-    ];
+    openssh.authorizedKeys.keys = getdata sshKeys [ "scott" ];
     extraGroups = [ "wheel" ];
     initialHashedPassword = "$y$j9T$NSMuZ83C3iGB1HqRhcZOy.$6CGZk2KH94gE/gjBro9vioOkOFJw.a4rhQKJI4HzBB9";
     packages = with pkgs; [
@@ -13,10 +14,7 @@
       vim
     ];
   };
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7hEnkX2r9nnIoVUa+isMwtdEppqWMTU9VDVE47ftLb MacBook"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9YD5mX4qfWS35Kcrk2hymaXlcSEUs3lWYa9bLKOcNW Pixels-PC"
-  ];
+  users.users.root.openssh.authorizedKeys.keys = getdata sshKeys [ "scott" ];
   users.mutableUsers = false;
   services.openssh.settings.AllowUsers = [ "pixel" "root" ];
   environment.etc.nixos.source = "/home/pixel/nix-server";
