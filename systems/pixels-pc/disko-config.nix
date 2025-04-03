badffdcb8952de670d94f9366d9d922f516bf161
Pixel <scott.blatt.0b10@gmail.com>
2025-04-02 20:36:45 -0500

    fix change issues

--- systems/pixels-pc/disko-config.nix
+++ systems/pixels-pc/disko-config.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 
-lib.internal.conf-templates.disko {
+lib.internal.confTemplates.disko {
   disk-id = "ata-Samsung_SSD_870_QVO_2TB_S5VWNJ0R105727V";
   esp-size = "1G";
   swap-size = "50G";
