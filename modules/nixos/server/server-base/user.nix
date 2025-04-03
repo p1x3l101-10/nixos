7dd3e898ea2299db7dcfc59a1804b14c0cdae7ad
Pixel <scott.blatt.0b10@gmail.com>
2025-04-02 22:21:20 -0500

    type error

--- modules/nixos/server/server-base/user.nix
+++ modules/nixos/server/server-base/user.nix
@@ -6,7 +6,7 @@ in
 {
   users.users.pixel = {
     isNormalUser = true;
-    openssh.authorizedKeys.keys = getdata sshKeys [ "scott" ];
+    openssh.authorizedKeys.keys = getdata "sshKeys" [ "scott" ];
     extraGroups = [ "wheel" ];
     initialHashedPassword = "$y$j9T$NSMuZ83C3iGB1HqRhcZOy.$6CGZk2KH94gE/gjBro9vioOkOFJw.a4rhQKJI4HzBB9";
     packages = with pkgs; [
@@ -14,7 +14,7 @@ in
       vim
     ];
   };
-  users.users.root.openssh.authorizedKeys.keys = getdata sshKeys [ "scott" ];
+  users.users.root.openssh.authorizedKeys.keys = getdata "sshKeys" [ "scott" ];
   users.mutableUsers = false;
   services.openssh.settings.AllowUsers = [ "pixel" "root" ];
   environment.etc.nixos.source = "/home/pixel/nix-server";
