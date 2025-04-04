ed351ddc5ec0272cff9e478523194fa367bf653e
Pixel <scott.blatt.0b10@gmail.com>
2025-04-04 16:43:05 -0500

    do is kinda borked atm

--- systems/do-droplet/default.nix
+++ systems/do-droplet/default.nix
@@ -1,7 +1,7 @@
 { lib, modulesPath, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ./base-min.nix ./config.nix ];
+  imports = [ ./hardware-configuration.nix ./base-min.nix ./config.nix ./reverse-vpn.nix ];
   networking.hostName = "do-droplet";
   environment.etc.machine-id.text = "98b8855dd64e40a39c4ab8cb54520895";
   networking.hostId = "98b8855d";
