becb0d02c1bc46bb93d097f6d7632611d9e5146c
Pixel <scott.blatt.0b10@gmail.com>
2025-04-09 17:42:24 -0500

    fix path

--- modules/nixos/server/server-base/container.nix
+++ modules/nixos/server/server-base/container.nix
@@ -26,5 +26,5 @@
     { directory = "/var/lib/machines"; mode = "0700"; }
     { directory = "/var/lib/containers"; mode = "0700"; }
   ];
-  environment.etc."/etc/containers/network".source = (pkgs.runCommand "empty-dir" {} "mkdir -p $out");
+  environment.etc."containers/network".source = (pkgs.runCommand "empty-dir" {} "mkdir -p $out");
 }
