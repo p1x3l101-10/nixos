3c50cbb02404f5f60c5799e405b76a7fc0c948c1
Pixel <scott.blatt.0b10@gmail.com>
2025-04-09 17:45:20 -0500

    fix nm dirs

--- modules/nixos/server/server-base/container.nix
+++ modules/nixos/server/server-base/container.nix
@@ -26,5 +26,8 @@
     { directory = "/var/lib/machines"; mode = "0700"; }
     { directory = "/var/lib/containers"; mode = "0700"; }
   ];
-  environment.etc."containers/network".source = (pkgs.runCommand "empty-dir" {} "mkdir -p $out");
+  environment.etc = {
+    "containers/network".source = (pkgs.runCommand "empty-dir" {} "mkdir -p $out");
+    "NetworkManager/system-connections".source = (pkgs.runCommand "empty-dir" {} "mkdir -p $out");
+  };
 }
