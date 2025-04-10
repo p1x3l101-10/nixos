19e75615f5bacb8b797ed0cd9722c10b02fcf3c8
Pixel <scott.blatt.0b10@gmail.com>
2025-04-09 19:21:34 -0500

    wierd escapes

--- packages/rebuild/default.nix
+++ packages/rebuild/default.nix
@@ -6,5 +6,5 @@
 writeShellScriptBin "rebuild" ''
   cd /etc/nixos
   ${gitMinimal}/bin/git pull
-  sudo nix shell nixpkgs#nix --command nixos-rebuild \${1:-"boot"}
+  sudo nix shell nixpkgs#nix --command nixos-rebuild $''+''{1:-"boot"}
 ''
