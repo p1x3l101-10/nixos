094ed83119718b297d98863cead437ea9a881796
Pixel <scott.blatt.0b10@gmail.com>
2025-04-09 19:48:42 -0500

    Bugfixes

--- modules/nixos/base/hideGetty.nix
+++ modules/nixos/base/hideGetty.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  systemd.maskedServices = (builtins.genList (i:
-    "getty@tty${toString (i + 1)}"
+  systemd.services = builtins.listToAttrs (builtins.genList (i: {
+    name = "getty@tty${toString (i + 1)}";
+    value = {
+      enable = lib.mkForce false;
+      wantedBy = [];
+      unitConfig = {
+        ConditionPathExists = "/nonexistent"; # prevent any accidental activation
+      };
+    };
+  }
   ) 5);
   systemd.services."getty@tty6".enable = true; # Ensure tty6 exists
 }
