197db65e848798d224b3596e55ee18ff0684fefd
Pixel <scott.blatt.0b10@gmail.com>
2025-04-14 15:29:05 -0500

    remove traces

--- systems/nixbook/systemd-sysupdate.nix
+++ systems/nixbook/systemd-sysupdate.nix
@@ -13,7 +13,7 @@ in {
     mkdir -p $out/${updateVersion}
     cp ${nixbookSystem.config.system.build.uki} $out/${updateVersion}/system.uki
     cp ${closure}/store-paths $out/${updateVersion}/closure.txt
-    nix copy --to file://$out/${updateVersion}/store --no-check-sigs $(cat ${closureTxt})
+    nix copy --to file://$out/${updateVersion}/store --no-check-sigs $(cat ${closure}/store-paths)
 
     cat > $out/${updateVersion}/update.conf <<EOF
 [Update]
