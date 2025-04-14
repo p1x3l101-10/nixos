6173948e53c47f43c4ebe3b21b0a172dd37a512b
Pixel <scott.blatt.0b10@gmail.com>
2025-04-14 15:28:47 -0500

    typo

--- systems/nixbook/systemd-sysupdate.nix
+++ systems/nixbook/systemd-sysupdate.nix
@@ -12,7 +12,7 @@ in {
   } ''
     mkdir -p $out/${updateVersion}
     cp ${nixbookSystem.config.system.build.uki} $out/${updateVersion}/system.uki
-    cp ${closureInfo}/store-paths $out/${updateVersion}/closure.txt
+    cp ${closure}/store-paths $out/${updateVersion}/closure.txt
     nix copy --to file://$out/${updateVersion}/store --no-check-sigs $(cat ${closureTxt})
 
     cat > $out/${updateVersion}/update.conf <<EOF
