bff0b74be9ce82f2333a9ec465024bf3764d370d
Pixel <scott.blatt.0b10@gmail.com>
2025-04-14 14:47:59 -0500

    cpu arches

--- systems/pixels-pc/cpu.nix
+++ systems/pixels-pc/cpu.nix
@@ -1,5 +1,9 @@
 { lib, ... }:
-{
+
+let
+  gccArches = [
+  ];
+in {
   /* Make builds the same again
     nixpkgs.system = lib.mkForce {
     features = [ "gccarch-znver3" ];
@@ -10,6 +14,8 @@
     };
     };
   */
+  # Add all supported compile targets
+  nixpkgs.buildPlatform.systemFeatures = lib.forEach (x: "gccarch-" + x) gccArches;
   hardware = {
     cpu.amd.ryzen-smu.enable = true;
     hardware.amdgpu.initrd.enable = true;
