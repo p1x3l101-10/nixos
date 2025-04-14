77349d7279bd162c02a2c83f429316a8698bfcb1
Pixel <scott.blatt.0b10@gmail.com>
2025-04-14 15:51:33 -0500

    fix store

--- systems/nixbook/systemd-sysupdate.nix
+++ systems/nixbook/systemd-sysupdate.nix
@@ -7,23 +7,6 @@ let
   };
   updateVersion = self.rev;
   UKI = nixbookSystem.config.system.build.uki + "/" + nixbookSystem.config.system.build.uki.name;
-  storeTarball = pkgs.runCommand "store.tar.xz" {
-    nativeBuildInputs = [ pkgs.gnutar pkgs.coreutils pkgs.findutils ];
-  } ''
-    mkdir -p tmp-root
-
-    while read path; do
-      if [ -d "$path" ]; then
-        # If it's a directory, recreate it explicitly with parents
-        mkdir -p "tmp-root$path"
-      else
-        # Copy file or symlink, preserving full store path
-        cp -a --parents "$path" tmp-root/
-      fi
-    done < ${closure}/store-paths
-
-    tar cvJf $out -C tmp-root .
-  '';
 in {
   system-update = pkgs.runCommand "nixbook-update-${updateVersion}" {
     nativeBuildInputs = [ pkgs.coreutils pkgs.nix ];
@@ -31,7 +14,10 @@ in {
     mkdir -p $out/${updateVersion}
     cp ${UKI} $out/${updateVersion}/system.uki
     cp ${closure}/store-paths $out/${updateVersion}/closure.txt
-    cp ${storeTarball} $out/${updateVersion}/store.tar.zstd
+    mkdir -p "$out/${updateVersion}/store"
+    while read path; do
+      cp -aL "$path" "$out/${updateVersion}/store/"
+    done < ${closureInfo}/store-paths
 
     cat > $out/${updateVersion}/update.conf <<EOF
 [Update]
