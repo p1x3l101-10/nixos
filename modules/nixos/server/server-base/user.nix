a5bd061e549c1161cd32bbb37b2bfd3a405e99a6
Pixel <scott.blatt.0b10@gmail.com>
2025-04-23 09:51:05 -0500

    fully use new system

--- modules/nixos/server/server-base/user.nix
+++ modules/nixos/server/server-base/user.nix
@@ -1,12 +1,9 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, userdata, ... }:
 
-let
-  getdata = key: names: (import ../userdata.nix { inherit lib; }).getdata key names;
-in
 {
   users.users.pixel = {
     isNormalUser = true;
-    openssh.authorizedKeys.keys = getdata "sshKeys" [ "scott" ];
+    openssh.authorizedKeys.keys = userdata "sshKeys" [ "scott" ];
     extraGroups = [ "wheel" ];
     initialHashedPassword = "$y$j9T$NSMuZ83C3iGB1HqRhcZOy.$6CGZk2KH94gE/gjBro9vioOkOFJw.a4rhQKJI4HzBB9";
     packages = with pkgs; [
@@ -16,7 +13,7 @@ in
     subUidRanges = [{ count = 65536; startuid = 100000; }];
     subGidRanges = [{ count = 65536; startuid = 100000; }];
   };
-  users.users.root.openssh.authorizedKeys.keys = getdata "sshKeys" [ "scott" ];
+  users.users.root.openssh.authorizedKeys.keys = userdata "sshKeys" [ "scott" ];
   users.mutableUsers = false;
   services.openssh.settings.AllowUsers = [ "pixel" "root" ];
   environment.etc.nixos.source = "/home/pixel/nixos";
