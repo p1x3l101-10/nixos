2b8d7ba6a80d0b3cf82e8c1b056920ecdd977373
Pixel <scott.blatt.0b10@gmail.com>
2025-04-23 09:44:23 -0500

    fix path

--- modules/nixos/server/default.nix
+++ modules/nixos/server/default.nix
@@ -3,7 +3,7 @@
 {
   # Add a data getter
   _module.args = {
-    userdata = key: names: (import ../userdata.nix { inherit lib; }).getdata key names;
+    userdata = key: names: (import ./userdata.nix { inherit lib; }).getdata key names;
   };
   imports = [
     ./factorio
