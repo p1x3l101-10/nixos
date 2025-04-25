337cbe65045f800d5941bb7218d08ea6b5352aac
Pixel <scott.blatt.0b10@gmail.com>
2025-04-25 13:01:59 -0500

    fix download

--- modules/nixos/server/minecraft/tinkers.nix
+++ modules/nixos/server/minecraft/tinkers.nix
@@ -9,7 +9,6 @@
           (pkgs.fetchzip {
             url = "https://pixeldrain.com/api/file/T7a45JRE";
             hash = "sha256-pvyElDBta/fTAGcFkM3rz00Zfi+AO0VTgqf+JxJOuTE=";
-            stripRoot=false;
             extension = "zip";
           })
         ];
