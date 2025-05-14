26517a3c5b9d9290e4c7eda86b4247b8227168e6
Pixel <scott.blatt.0b10@gmail.com>
2025-05-14 14:44:53 -0500

    fix wrapper

--- packages/sculptor/default.nix
+++ packages/sculptor/default.nix
@@ -1,52 +1,72 @@
 { lib
-, fetchFromGitHub
 , rustPlatform
+, stdenv
+, buildEnv
+, fetchFromGitHub
 , makeWrapper
 , pkg-config
 , openssl
 }:
 
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "sculptor";
-  version = "0.4.0";
+let
+  sculptor-bin = rustPlatform.buildRustPackage (finalAttrs: {
+    pname = "sculptor-bin";
+    version = "0.4.0";
 
-  src = fetchFromGitHub {
-    owner = "shiroyashik";
-    repo = finalAttrs.pname;
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-+Jqvxi1v09g686A6bJ+mI6tp9WSYpHSU0MD8j1chV54=";
+    src = fetchFromGitHub {
+      owner = "shiroyashik";
+      repo = finalAttrs.pname;
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-+Jqvxi1v09g686A6bJ+mI6tp9WSYpHSU0MD8j1chV54=";
+    };
+
+    buildInputs = [
+      openssl
+    ];
+
+    nativeBuildInputs = [
+      openssl.dev
+      pkg-config
+    ];
+
+    cargoHash = "sha256-N7j0zKeCrBw6PzfXYnHfn/4Q29bMOzfeWaKrf9ZzZPk=";
+
+    # There are no tests
+    doCheck = false;
+
+    meta = with lib; {
+      name = "Sculptor";
+      description = "Unofficial backend V2 for minecraft mod Figura";
+      homepage = "https://github.com/shiroyashik/sculptor?";
+      changelog = "https://github.com/shiroyashik/sculptor/releases/tag/v${finalAttrs.version}";
+      license = lib.licenses.gpl3;
+      mainProgram = "sculptor";
+      platforms = lib.platforms.unix;
+    };
+  });
+  sculptor-data = stdenv.mkDerivation {
+    pname = "sculptor-data-files";
+    inherit (sculptor-bin) version src;
+    installPhase = ''
+      mkdir -p $out/share/factory/etc/sculptor
+      cp ./Config.toml.example $out/share/factory/etc/sculptor/Config.toml
+    '';
   };
+in
 
+buildEnv {
+  name = "sculptor";
+  paths = [
+    sculptor-bin
+  ];
   buildInputs = [
-    openssl
-  ];
-
-  nativeBuildInputs = [
     makeWrapper
-    openssl.dev
-    pkg-config
   ];
-
-  postInstallPhase = ''
+  postBuild = ''
     wrapProgram $out/bin/sculptor \
-      --set RUST_CONFIG "/etc/sculptor/Config.toml" \
-      --set ASSETS_FOLDER "/var/lib/sculptor/assets" \
-      --set AVATARS_FOLDER "/var/lib/sculptor/avatars" \
-      --set LOGS_FOLDER "/var/log/sculptor"
+        --set RUST_CONFIG "/etc/sculptor/Config.toml" \
+        --set ASSETS_FOLDER "/var/lib/sculptor/assets" \
+        --set AVATARS_FOLDER "/var/lib/sculptor/avatars" \
+        --set LOGS_FOLDER "/var/log/sculptor"
   '';
-
-  cargoHash = "sha256-N7j0zKeCrBw6PzfXYnHfn/4Q29bMOzfeWaKrf9ZzZPk=";
-
-  # There are no tests
-  doCheck = false;
-
-  meta = with lib; {
-    name = "Sculptor";
-    description = "Unofficial backend V2 for minecraft mod Figura";
-    homepage = "https://github.com/shiroyashik/sculptor?";
-    changelog = "https://github.com/shiroyashik/sculptor/releases/tag/v${finalAttrs.version}";
-    license = lib.licenses.gpl3;
-    mainProgram = "sculptor";
-    platforms = lib.platforms.unix;
-  };
-})
+}
