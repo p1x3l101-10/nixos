31239a3a549d995bd5462dcd93e7f8ff5611630d
Pixel <scott.blatt.0b10@gmail.com>
2025-05-14 18:26:18 -0500

    manual package call

--- modules/nixos/server/sculptor/sculptor.nix
+++ modules/nixos/server/sculptor/sculptor.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.sculptor = {
     enable = true;
+    package = pkgs.callPackage {} ../../../../packages/sculptor;
     openFirewall = true;
     config = {
       listen.port = 25575;
