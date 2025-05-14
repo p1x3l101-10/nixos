13617a96bc5b65c79df3228dd32268bac3643b7d
Pixel <scott.blatt.0b10@gmail.com>
2025-05-14 15:23:29 -0500

    preserve data

--- modules/nixos/server/sculptor/default.nix
+++ modules/nixos/server/sculptor/default.nix
@@ -8,4 +8,7 @@
       listen.port = 25575; # Exposed on 25575
     };
   };
+  environment.persistence."/nix/host/state/Servers/Sculptor/".directories = [
+    { directory = "/var/lib/private/sculptor"; mode = "0700"; }
+  ];
 }
