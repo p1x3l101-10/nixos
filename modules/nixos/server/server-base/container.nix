5884ec5806c5cc4b493bd7f5ca61cc9928e7af51
Pixel <scott.blatt.0b10@gmail.com>
2025-05-14 20:06:27 -0500

    cleanup

--- modules/nixos/server/server-base/container.nix
+++ modules/nixos/server/server-base/container.nix
@@ -9,7 +9,7 @@
   networking = {
     nat = {
       enable = true;
-      internalInterfaces = [ "ve-+" "vb-+" "br0" "veth0" ];
+      internalInterfaces = [ "ve-+" "vb-+" "br0" ];
       externalInterface = "enp2s0";
       enableIPv6 = true;
     };
