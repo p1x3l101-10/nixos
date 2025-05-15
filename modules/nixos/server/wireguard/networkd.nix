d83ec2d7d52372744212a1d34c9da3d8f196b713
Pixel <scott.blatt.0b10@gmail.com>
2025-05-15 18:35:43 -0500

    fix WG

--- modules/nixos/server/wireguard/networkd.nix
+++ modules/nixos/server/wireguard/networkd.nix
@@ -11,7 +11,6 @@ in {
         netdevConfig = {
           Kind = "wireguard";
           Name = "wg0";
-          MTUBytes = "1300";
         };
         wireguardConfig = {
           PrivateKeyFile = "/nix/host/keys/wireguard/psk";
@@ -25,29 +24,12 @@ in {
               "0.0.0.0/0"
             ];
             Endpoint = wgEndpoint;
+            PersistentKeepalive = 25;
           }
         ];
       };
     };
     networks = {
-      "99-wg0" = {
-        matchConfig.Name = "wg0";
-        address = [
-          "fe80::3/64"
-          "fc00::3/120"
-          "10.100.0.2/24"
-        ];
-        DHCP = "no";
-        dns = ["fc00::53"];
-        ntp = ["fc00::123"];
-        gateway = [
-          "fc00::1"
-          "10.100.0.1"
-        ];
-        networkConfig = {
-          IPv6AcceptRA = false;
-        };
-      };
       "99-wg0" = {
         matchConfig.Name = "wg0";
         networkConfig = {
