587860de7a314ddd44cedcca42127946289d0c4d
Pixel <scott.blatt.0b10@gmail.com>
2025-05-15 17:58:31 -0500

    reparent

--- modules/nixos/server/wireguard/networkd.nix
+++ modules/nixos/server/wireguard/networkd.nix
@@ -21,23 +21,23 @@
         }
       ];
     };
-  };
-  networks.wg0 = {
-    matchConfig.Name = "wg0";
-    address = [
-      "fe80::3/64"
-      "fc00::3/120"
-      "10.100.0.2/24"
-    ];
-    DHCP = "no";
-    dns = ["fc00::53"];
-    ntp = ["fc00::123"];
-    gateway = [
-      "fc00::1"
-      "10.100.0.1"
-    ];
-    networkConfig = {
-      IPv6AcceptRA = false;
+    networks.wg0 = {
+      matchConfig.Name = "wg0";
+      address = [
+        "fe80::3/64"
+        "fc00::3/120"
+        "10.100.0.2/24"
+      ];
+      DHCP = "no";
+      dns = ["fc00::53"];
+      ntp = ["fc00::123"];
+      gateway = [
+        "fc00::1"
+        "10.100.0.1"
+      ];
+      networkConfig = {
+        IPv6AcceptRA = false;
+      };
     };
   };
 }
