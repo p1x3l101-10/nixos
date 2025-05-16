806688f5f0cb673e0ddb5f96ea91d5e45ed9cea7
Pixel <scott.blatt.0b10@gmail.com>
2025-05-15 22:32:46 -0500

    waiting game

--- modules/nixos/server/sculptor/nginx.nix
+++ modules/nixos/server/sculptor/nginx.nix
@@ -4,7 +4,7 @@
   services.nginx = {
     virtualHosts."sculptor.piplup.pp.ua" = {
       onlySSL = true;
-      enableACME = true;
+      enableACME = false; # Wait until dns finally updates
       locations."/" = {
         proxyPass = "http://127.0.0.1:25575";
         proxyWebsockets = true;
