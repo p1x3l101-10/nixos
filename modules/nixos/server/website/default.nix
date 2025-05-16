b5cc2b2de9fd113ddf21841b8200d34a55881fcf
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 17:14:00 -0500

    new formatter

--- modules/nixos/server/website/default.nix
+++ modules/nixos/server/website/default.nix
@@ -1,11 +1,13 @@
-{ globals, ... }:
+{ globals, pkgs, ... }:
 
 {
   config = globals.server.dns.required {
     services.nginx.virtualHosts."${globals.server.dns.basename}" = {
       addSSL = true;
       enableACME = true;
-      locations."/".root = ./webpage;
+      locations."/".root = pkgs.internal.aya.build {
+        src = ./webpage;
+      };
     };
   };
 }
