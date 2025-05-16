4c4f2d920fd0cdda0cfb16c72b2bb40c8df6996a
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 13:23:26 -0500

    add more algos

--- modules/nixos/server/server-base/ssh.nix
+++ modules/nixos/server/server-base/ssh.nix
@@ -2,6 +2,15 @@
 
 {
   services.openssh = {
-    settings.PermitRootLogin = lib.mkForce "yes";
+    settings = {
+      PermitRootLogin = lib.mkForce "yes";
+      KexAlgorithms = [
+        "sntrup761x25519-sha512@openssh.com"
+        "curve25519-sha256"
+        "curve25519-sha256@libssh.org"
+        "diffie-hellman-group-exchange-sha256"
+        "ssh-rsa-cert-v01@openssh.com"
+      ];
+    };
   };
 }
