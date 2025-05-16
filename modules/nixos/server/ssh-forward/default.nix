a9e16ace735ca2276123d38906d4bb5503e58d57
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 15:49:44 -0500

    fix key?

--- modules/nixos/server/ssh-forward/default.nix
+++ modules/nixos/server/ssh-forward/default.nix
@@ -7,7 +7,7 @@
   networking.sshForwarding = {
     enable = true;
     trustedHostKeys = [
-      "piplup.pp.ua ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCu2YSKzbmW2D/Iq8oxAVqeHZ0CXGmnRa++uWaeV3sQmUS82v0yQXjOlXHY6hqRZlqkE2lM0FGvLxzPTBnls1aRFsR1dzVm2/efyfxi1Pv+7S0Gi96gisMiy+U2/bRUI/Yq0c6jpMZGCEAiiZtcexKLIlewTJwM5SQFd3kIk35kCIA2UfiZ5XUlfcURg7Gc1OMQw3wRP7F1zGy/NFxZUn1YbVZx+uRmP65YyvMtXek0coKuoAdjvR+7/rXDMCPwDIje4lYPDp/vt9bigXbkxc7uPAmtBq2hWZcfoV7ro9X30Ty/YlYn5h536tz0s6/8x/4/2u0W0SV3QQpGFRWBqIYX"
+      "[piplup.pp.ua]:2222 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDu8VS/AEY5smqFoxM+hRKoCp5cTofBcNKhgyni1qk4A5XKTPpn5YTpbGRMPx5Xsl4xp3Q5y/gs3Y+j+qvzE+A0="
     ];
     sshPort = 2222;
     keyFile = "/nix/host/keys/ssh-tunnel/id.key";
