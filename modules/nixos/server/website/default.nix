9eb58731e3ae0344e53ede2982e64213ffcadc65
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 17:16:55 -0500

    stupid recursions

--- modules/nixos/server/website/default.nix
+++ modules/nixos/server/website/default.nix
@@ -1,13 +1,11 @@
 { globals, pkgs, ... }:
 
 {
-  config = globals.server.dns.required {
-    services.nginx.virtualHosts."${globals.server.dns.basename}" = {
-      addSSL = true;
-      enableACME = true;
-      locations."/".root = pkgs.internal.aya.build {
-        src = ./webpage;
-      };
+  services.nginx.virtualHosts."${globals.server.dns.basename}" = globals.server.dns.required {
+    addSSL = true;
+    enableACME = true;
+    locations."/".root = pkgs.internal.aya.build {
+      src = ./webpage;
     };
   };
 }
