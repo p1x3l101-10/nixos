7128b61d2f256b9ddcb9931189171e810e98fa99
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 09:06:49 -0500

    screwed that up lol

--- modules/nixos/server/ssh-forward/default.nix
+++ modules/nixos/server/ssh-forward/default.nix
@@ -9,7 +9,7 @@
     serviceConfig = {
       Restart = "on-failure";
       RestartSec = 5;
-      ExecStart = "${pkgs.openssh}/bin/ssh -NTC -vvv -i /nix/host/keys/ssh-tunnel/id.key -o StrictHostKeychecking=no -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -R 80:127.0.0.1:8080 -R 443:127.0.0.1:4443 -R 25565:127.0.0.1:25565 proxy@piplup.pp.ua";
+      ExecStart = "${pkgs.openssh}/bin/ssh -NTC -vvv -i /nix/host/keys/ssh-tunnel/id.key -o StrictHostKeychecking=no -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -R 8080:127.0.0.1:80 -R 4443:127.0.0.1:443 -R 25565:127.0.0.1:25565 proxy@piplup.pp.ua";
     };
   };
 }
