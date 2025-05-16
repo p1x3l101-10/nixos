dd5d0fb6c2c597b5924a49f30616c6409e52f2f3
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 18:51:02 -0500

    persist data next time

--- modules/nixos/server/nginx/acme.nix
+++ modules/nixos/server/nginx/acme.nix
@@ -5,4 +5,7 @@
     acceptTerms = true;
     defaults.email = "scott.blatt.0b10@gmail.com";
   };
+  environment.persistence."/nix/host/state/Servers/ACME-Certs".directories = [
+    "/var/lib/acme"
+  ];
 }
