314de9459279b42e71d6384f565c76bc980046ad
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 15:30:25 -0500

    modularize the tunnel

--- modules/nixos/server/ssh-forward/default.nix
+++ modules/nixos/server/ssh-forward/default.nix
@@ -1,15 +1,15 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  systemd.services.ssh-tunnel = {
-    description = "Expose local ports on remote server";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    restartIfChanged = true;
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = 5;
-      ExecStart = "${pkgs.openssh}/bin/ssh -NTC -vvv -i /nix/host/keys/ssh-tunnel/id.key -o StrictHostKeychecking=no -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -R 8080:127.0.0.1:80 -R 4443:127.0.0.1:443 -R 25565:127.0.0.1:25565 proxy@piplup.pp.ua";
-    };
+  imports = [
+    ./module.nix
+  ];
+  networking.sshForwarding = {
+    enable = true;
+    trustedHostKeys = [
+      "piplup.pp.ua ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCu2YSKzbmW2D/Iq8oxAVqeHZ0CXGmnRa++uWaeV3sQmUS82v0yQXjOlXHY6hqRZlqkE2lM0FGvLxzPTBnls1aRFsR1dzVm2/efyfxi1Pv+7S0Gi96gisMiy+U2/bRUI/Yq0c6jpMZGCEAiiZtcexKLIlewTJwM5SQFd3kIk35kCIA2UfiZ5XUlfcURg7Gc1OMQw3wRP7F1zGy/NFxZUn1YbVZx+uRmP65YyvMtXek0coKuoAdjvR+7/rXDMCPwDIje4lYPDp/vt9bigXbkxc7uPAmtBq2hWZcfoV7ro9X30Ty/YlYn5h536tz0s6/8x/4/2u0W0SV3QQpGFRWBqIYX"
+    ];
+    sshPort = 2222;
+    keyFile = "/nix/host/keys/ssh-tunnel/id.key";
   };
 }
---
+++ modules/nixos/server/ssh-forward/module.nix
@@ -0,0 +1,78 @@
+{ config, options, pkgs, lib, globals, ... }:
+
+let
+  cfg = config.networking.sshForwarding;
+  portFormat = _: {
+    options = with lib; {
+      host = mkOption {
+        description = "The host port to expose";
+        type = types.port;
+      };
+      remote = mkOption {
+        description = "The remote port to listen on";
+        type = types.port;
+        default = self.config.host;
+      };
+    };
+  };
+in {
+  options.networking.sshForwarding = with lib; {
+    enable = mkEnableOption "ssh forwarding server";
+    proxyUser = mkOption {
+      type = types.str;
+      description = "The user that will accept the connection";
+      default = "proxy";
+    };
+    trustedHostKeys = mkOption {
+      description = "The host's public key";
+      type = with types; listOf str;
+      default = [];
+    };
+    sshPort = mkOption {
+      description = "ssh port to connect to";
+      type = types.port;
+      default = 22;
+    };
+    keyFile = mkOption {
+      type = types.str;
+      description = "The path to the ssh key file to be used";
+      default = "";
+    };
+    ports = mkOption {
+      type = with types; listOf (coercedTo port (host: { inherit host; }) (submodule portFormat));
+      description = "List of ports to forward";
+      default = [];
+    };
+  };
+  config = lib.mkIf cfg.enable (lib.mkIf globals.vps.enabled { # Enable when the module is requested and when the vps exists
+    systemd.services.ssh-tunnel = {
+      description = "Expose local ports on remote server";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      restartIfChanged = true;
+      script = let
+        portArgs = lib.forEach cfg.ports (x:
+          "-R ${toString x.remote}:127.0.0.1:${toString x.host}"
+        );
+      in ''
+        ${pkgs.openssh}/bin/ssh \
+          -NTC \
+          -vvv \
+          -i /nix/host/keys/ssh-tunnel/id.key \
+          -o UserKnownHostsFile=${toString (pkgs.writeTextFile {
+            name = "known_hosts";
+            text = lib.lists.concatStringsSep "\n" cfg.trustedHostKeys;
+          })} \
+          -o ServerAliveInterval=60 \
+          -o ExitOnForwardFailure=yes \
+          ${lib.concatStringsSep " " portArgs} \
+          ${cfg.proxyUser}@${globals.vps.get} \
+          -p ${cfg.sshPort}
+      '';
+      serviceConfig = {
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
+    };
+  });
+}
