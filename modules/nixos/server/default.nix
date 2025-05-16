d64a095b0a17cefa4c5a244c2593fb244a17b12d
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 12:16:24 -0500

    dont use unneeded serviecs

--- modules/nixos/server/default.nix
+++ modules/nixos/server/default.nix
@@ -6,16 +6,16 @@
     userdata = key: names: (import ./userdata.nix { inherit lib; }).getdata key names;
   };
   imports = [
-    ./factorio
-    ./ftp
+    #./factorio
+    #./ftp
     ./minecraft
-    ./nextcloud
+    #./nextcloud
     ./nginx
-    ./nix-serve
-    ./s3
+    #./nix-serve
+    #./s3
     ./sculptor
     ./server-base
     ./ssh-forward
-    ./terraria
+    #./terraria
   ];
 }
