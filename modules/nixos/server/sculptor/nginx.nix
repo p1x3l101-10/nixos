4919d3b101123884143aea8b3b62798f108d8138
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 12:08:01 -0500

    pretty

--- modules/nixos/server/sculptor/nginx.nix
+++ modules/nixos/server/sculptor/nginx.nix
@@ -1,13 +1,11 @@
 { config, ... }:
 
 {
-  services.nginx = {
-    virtualHosts."sculptor.piplup.pp.ua" = {
-      enableACME = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:25575";
-        proxyWebsockets = true;
-      };
+  services.nginx.virtualHosts."sculptor.piplup.pp.ua" = {
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:25575";
+      proxyWebsockets = true;
     };
   };
 }
