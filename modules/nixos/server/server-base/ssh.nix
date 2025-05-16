8ac95257d6fc04c6ae89115f67d75335505fcc74
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 13:27:20 -0500

    new key

--- modules/nixos/server/server-base/ssh.nix
+++ modules/nixos/server/server-base/ssh.nix
@@ -4,13 +4,17 @@
   services.openssh = {
     settings = {
       PermitRootLogin = lib.mkForce "yes";
-      KexAlgorithms = [
-        "sntrup761x25519-sha512@openssh.com"
-        "curve25519-sha256"
-        "curve25519-sha256@libssh.org"
-        "diffie-hellman-group-exchange-sha256"
-        "ssh-rsa-cert-v01@openssh.com"
-      ];
     };
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/nix/host/keys/ssh/rsa.key";
+        type = "rsa";
+      }
+      {
+        path = "/nix/host/keys/ssh/ed25519.key";
+        type = "ed25519";
+      }
+    ];
   };
 }
