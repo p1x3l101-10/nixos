9c1da61832da6340a40e47e1188e7cfd6c6be286
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 15:06:56 -0500

    more vps flags

--- modules/nixos/server/globals.nix
+++ modules/nixos/server/globals.nix
@@ -4,17 +4,18 @@ lib.fix (self: {
   server = {
     dns = { # DNS data and helpers
       basename = "piplup.pp.ua"; # Basename for dns
-      exists = (if self.server.basename != "" then true else false); # True when dns is not empty, otherwise false
+      exists = (if self.server.dns.basename != "" then true else false); # True when dns is not empty, otherwise false
       required = attrs: (lib.mkIf self.server.dns.exists attrs); # mkIf wrapper to check if the dns is set or not
       requiredList = list: (lib.optionals self.server.dns.exists list) # Optionals wrapper to do the same but for lists
     };
   };
   vps = {
+    enabled = true;
     ip = "";
     dns = self.server.dns.basename;
-    valid = self.server.dns.exists;
+    validDomain = (if self.vps.dns != "" then true else false);
     get = (
-      if self.vps.valid then
+      if self.vps.validDomain then
         self.vps.dns
       else
         self.vps.ip
