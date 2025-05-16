3d1b23a30220e068f3c2c1e24662b8163e46e2ba
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 18:36:22 -0500

    redir

--- modules/nixos/server/nginx/nginx.nix
+++ modules/nixos/server/nginx/nginx.nix
@@ -1,14 +1,21 @@
-{ lib, globals, ... }:
+{ config, lib, globals, ... }:
 
 {
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = globals.server.dns.exists;
-    virtualHosts."_" = { # The default landing page
-      enableACME = lib.mkDefault false;
-      locations."/".root = lib.mkDefault ./landing;
-    };
+    virtualHosts."_" = if
+        (config.services.nginx.virtualHosts."${globals.server.dns.basename}" == {}) # Redirect to normal website when it actually exists
+      then
+        { # The default landing page
+          enableACME = lib.mkDefault false;
+          locations."/".root = lib.mkDefault ./landing;
+        }
+      else {
+        globalRedirect = globals.server.dns.basename;
+      }
+    ;
   };
   networking.sshForwarding.ports = [
     { host = 80; remote = 8080; }
