65f216ffcacbe21cc7a72adf3c53232cd61db58f
Pixel <scott.blatt.0b10@gmail.com>
2025-05-15 22:25:21 -0500

    use pkgs

--- modules/nixos/server/ssh-forward/default.nix
+++ modules/nixos/server/ssh-forward/default.nix
@@ -9,7 +9,7 @@
     serviceConfig = {
       Restart = "on-failure";
       RestartSec = 5;
-      ExecStart = "${openssh}/bin/ssh -NTC -i /nix/host/keys/ssh-tunnel/id.key -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -R 80:127.0.0.1:80 -R 443:127.0.0.1:443 -R 25565:127.0.0.1:25565 -R 22:127.0.0.1:2222 proxy@piplup.pp.ua";
+      ExecStart = "${pkgs.openssh}/bin/ssh -NTC -i /nix/host/keys/ssh-tunnel/id.key -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -R 80:127.0.0.1:80 -R 443:127.0.0.1:443 -R 25565:127.0.0.1:25565 -R 22:127.0.0.1:2222 proxy@piplup.pp.ua";
     };
   };
 }
