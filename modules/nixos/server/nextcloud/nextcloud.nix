ffebb2cfb9b00abf76f20f9d412d05f43449758f
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 22:51:27 -0500

    let NC manage its apps

--- modules/nixos/server/nextcloud/nextcloud.nix
+++ modules/nixos/server/nextcloud/nextcloud.nix
@@ -6,26 +6,6 @@
     package = pkgs.nextcloud31;
     hostName = "nextcloud.${globals.server.dns.basename}";
     https = true;
-    extraApps = {
-      inherit (self.package.packages.apps)
-        news
-        contacts
-        calendar
-        cookbook
-        cospend
-        deck
-        end_to_end_encryption
-        forms
-        mail
-        notes
-        onlyoffice
-        polls
-        quota_warning
-        spreed
-        bookmarks
-        tasks
-      ;
-    };
     extraAppsEnable = true;
     config = {
       dbtype = "sqlite";
