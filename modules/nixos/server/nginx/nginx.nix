cb5bf6d1e2624f4e1c02db589319462d66181be3
Pixel <scott.blatt.0b10@gmail.com>
2025-05-17 01:22:59 -0500

    typo

--- modules/nixos/server/nginx/nginx.nix
+++ modules/nixos/server/nginx/nginx.nix
@@ -6,7 +6,7 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = globals.server.dns.exists;
     recommendedGzipSettings = true;
-    recommendedOptimisationSettings = true;
+    recommendedOptimizationSettings = true;
     virtualHosts."_" = (
       if
         globals.server.www.exists # Redirect to normal website when it actually exists
