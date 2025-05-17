3e09776ea2d81896c913b0055879fa892539a954
Pixel <scott.blatt.0b10@gmail.com>
2025-05-17 17:17:49 -0500

    unbreak nc

--- modules/nixos/server/nextcloud/nginx.nix
+++ modules/nixos/server/nextcloud/nginx.nix
@@ -4,5 +4,13 @@
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = globals.server.dns.required {
     forceSSL = true;
     enableACME = true;
+    extraConfig = ''
+      # kill cache
+      add_header Last-Modified $date_gmt;
+      add_header Cache-Control 'no-store, no-cache';
+      if_modified_since off;
+      expires off;
+      etag off;
+    '';
   };
 }
