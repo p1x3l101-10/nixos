87385ed4987e249c1243b8f56e55065c6f561dca
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 22:55:38 -0500

    fine, ill keep it

--- modules/nixos/server/nextcloud/nextcloud.nix
+++ modules/nixos/server/nextcloud/nextcloud.nix
@@ -9,6 +9,8 @@
     appstoreEnable = true;
     config = {
       dbtype = "sqlite";
+      adminuser = "internal-admin";
+      adminpassFile = "/nix/host/keys/nextcloud/admin-password.txt";
     };
   });
   environment.persistence."/nix/host/state/Servers/Nextcloud".directories = [
