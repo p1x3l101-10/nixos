8455528e611b8790ec3dd4759d4a8c76b77b4caa
Pixel <scott.blatt.0b10@gmail.com>
2025-05-17 01:23:58 -0500

    reparent

--- modules/nixos/server/matrix/nginx.nix
+++ modules/nixos/server/matrix/nginx.nix
@@ -6,7 +6,7 @@
       forceSSL = true;
       enableACME = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.matrix-conduit.port}";
+        proxyPass = "http://127.0.0.1:${toString config.services.matrix-conduit.settings.global.port}";
         proxyWebsockets = true;
       };
     };
