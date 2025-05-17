e54e8302207bfe6725968996f12360652ea0a356
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 22:55:07 -0500

    typo

--- modules/nixos/server/nextcloud/nextcloud.nix
+++ modules/nixos/server/nextcloud/nextcloud.nix
@@ -6,7 +6,7 @@
     package = pkgs.nextcloud31;
     hostName = "nextcloud.${globals.server.dns.basename}";
     https = true;
-    appstoreEnable = true
+    appstoreEnable = true;
     config = {
       dbtype = "sqlite";
     };
