f12f6138600d051161913729c9b158031241caa9
Pixel <scott.blatt.0b10@gmail.com>
2025-05-17 16:45:31 -0500

    index

--- modules/nixos/server/cdn/default.nix
+++ modules/nixos/server/cdn/default.nix
@@ -4,7 +4,12 @@
   services.nginx.virtualHosts."cdn.${globals.server.dns.basename}" = globals.server.dns.required {
     addSSL = true;
     enableACME = true;
-    locations."/".root = "/var/lib/http-cdn";
+    locations."/" = {
+      root = "/var/lib/http-cdn";
+      extraConfig = ''
+        autoindex on;
+      '';
+    };
   };
   environment.persistence."/nix/host/state/Servers/HTTP-CDN".directories = [
     { directory = "/var/lib/http-cdn"; mode = "0770"; group = "cdn"; }
