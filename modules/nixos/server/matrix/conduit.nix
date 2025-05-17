375d242dc85312f179e2bb7b25bd44f2b06bba90
Pixel <scott.blatt.0b10@gmail.com>
2025-05-17 18:22:17 -0500

    disable registration

--- modules/nixos/server/matrix/conduit.nix
+++ modules/nixos/server/matrix/conduit.nix
@@ -5,9 +5,7 @@
     enable = globals.server.dns.exists;
     settings.global = {
       address = "127.0.0.1"; 
-      # Change this to `false` after the first user (admin) is registered,
-      # and then run `$ nixos-rebuild switch`.
-      allow_registration = true;
+      allow_registration = false;
       registration_token = "opensesame";
       database_backend = "rocksdb";
       port = 6167;
