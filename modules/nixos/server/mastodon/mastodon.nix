ebcbb96178f986d4487cea3ac48e1f491289c006
Pixel <scott.blatt.0b10@gmail.com>
2025-05-16 21:01:55 -0500

    email (fake)

--- modules/nixos/server/mastodon/mastodon.nix
+++ modules/nixos/server/mastodon/mastodon.nix
@@ -6,6 +6,7 @@
     localDomain = "mastodon.${globals.server.dns.basename}";
     extraConfig.SINGLE_USER_MODE = "true";
     streamingProcesses = 3;
+    smtp.fromAddress = "noreply@mastodon.${globals.server.dns.basename}";
   };
   environment.persistence."/nix/host/state/Servers/Mastodon".directories = [
     "/var/lib/mastodon"
