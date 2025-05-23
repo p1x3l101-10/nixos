a35b7b1e749c2d8ee2b472a9b2691c6c050ab6ab
Pixel <scott.blatt.0b10@gmail.com>
2025-05-23 18:26:28 -0500

    gate for safety

--- modules/nixos/desktop/vr-min.nix
+++ modules/nixos/desktop/vr-min.nix
@@ -1,6 +1,6 @@
-{ ... }:
+{ lib, ... }:
 
-{
+lib.mkIf (config.networking.hostName == "pixels-pc") {
   networking.firewall = {
     allowedUDPPorts = [ 9757 ];
     allowedTCPPorts = [ 5353 9757 ];
