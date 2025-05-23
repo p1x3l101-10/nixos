71de73cb0eb121a59a62b5680e8521ba1a115b72
Pixel <scott.blatt.0b10@gmail.com>
2025-05-23 18:52:09 -0500

    more stuff

--- modules/nixos/desktop/vr-min.nix
+++ modules/nixos/desktop/vr-min.nix
@@ -2,7 +2,31 @@
 
 lib.mkIf (config.networking.hostName == "pixels-pc") {
   networking.firewall = {
-    allowedUDPPorts = [ 9757 ];
+    allowedUDPPorts = [ 9943 9944 9945 9946 9947 9949 5353 ];
     allowedTCPPorts = [ 5353 9757 ];
+  };
+  boot.kernelPatches = [
+    {
+      name = "amdgpu-ignore-ctx-privileges";
+      patch = pkgs.fetchpatch {
+        name = "cap_sys_nice_begone.patch";
+        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+      };
+    }
+  ];
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  nixpkgs.config = {
+    xr.enable = true;
+    rocmSupport = true;
+  };
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    openFirewall = true;
   };
 }
