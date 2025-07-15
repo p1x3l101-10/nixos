2e9f1c5d67b23c937a2c0f304065cc4f87b9f36c
Pixel <scott.blatt.0b10@gmail.com>
2025-07-15 15:22:45 -0500

    update for consistency

--- modules/nixos/desktop/tablet.nix
+++ modules/nixos/desktop/tablet.nix
@@ -1,8 +1,8 @@
-{ ... }:
+{ config, ... }:
 
 {
   users.groups.tablet = {};
   services.udev.extraRules = ''
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="tablet"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="${users.groups.tablet.name}"
   '';
 }
