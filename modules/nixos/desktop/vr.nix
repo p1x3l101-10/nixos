0fa077d4ce68cd5cd1e47a402d7441954d7307a7
Pixel <scott.blatt.0b10@gmail.com>
2025-07-15 18:17:29 -0500

    no autostart and no config

--- modules/nixos/desktop/vr.nix
+++ modules/nixos/desktop/vr.nix
@@ -13,31 +13,7 @@ lib.mkIf (config.networking.hostName == "pixels-pc") {
     enable = true;
     openFirewall = true;
     defaultRuntime = true;
-    autoStart = true;
-    config = {
-      enable = true;
-      json = {
-        # Autostart wlx-overlay
-        application = [
-          pkgs.wlx-overlay-s
-        ];
-        # 1.0x foveation scaling
-        scale = 1.0;
-        # 100 Mb/s
-        bitrate = 100000000;
-        encoders = [
-          {
-            encoder = "vaapi";
-            codec = "h265";
-            # 1.0 x 1.0 scaling
-            width = 1.0;
-            height = 1.0;
-            offset_x = 0.0;
-            offset_y = 0.0;
-          }
-        ];
-      };
-    };
+    autoStart = false;
   };
   # Envision
   programs.envision = {
