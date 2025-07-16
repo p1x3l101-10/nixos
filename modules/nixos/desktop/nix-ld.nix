6643e144dec8157e062a929074e97239bf6b6f43
Pixel <scott.blatt.0b10@gmail.com>
2025-07-15 19:44:41 -0500

    libs

--- modules/nixos/desktop/nix-ld.nix
+++ modules/nixos/desktop/nix-ld.nix
@@ -2,7 +2,21 @@
 {
   programs.nix-ld = {
     enable = true;
-    #libraries = [];
+    libraries = with pkgs; [
+      alsa-lib.out
+      dbus.lib
+      libGL.out
+      fontconfig.lib
+      openxr-loader.out
+      pipewire.out
+      libgcc.lib
+      kdePackages.wayland.out
+      xorg.libX11.out
+      xorg.libxcb.out
+      xorg.libXext.out
+      libxkbcommon.out
+      xorg.libXrandr.out
+    ];
   };
   environment.systemPackages = [
     inputs.nix-autobahn.packages.x86_64-linux.nix-autobahn
