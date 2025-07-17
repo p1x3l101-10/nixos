614fe0e90c6a8993d2af556d28d484f79455be2f
Pixel <scott.blatt.0b10@gmail.com>
2025-07-17 14:06:13 -0500

    fix showing

--- lib/flake/genPackages.nix
+++ lib/flake/genPackages.nix
@@ -2,26 +2,33 @@
 
 src: newScope: extraArgs:
 
-lib.makeScope newScope (self:
-(
-  lib.attrsets.mapAttrs
-    (name: value:
-    (self.callPackage value extraArgs)
-    )
-    (
-      lib.attrsets.mapAttrs
-        (name: _:
-        src + "/${name}"
-        )
-        (
-          lib.attrsets.filterAttrs
-            (_: type:
-            type == "directory"
-            )
-            (
-              builtins.readDir src
-            )
-        )
-    )
-)
-)
+builtins.removeAttrs (
+  lib.makeScope newScope (self:
+  (
+    lib.attrsets.mapAttrs
+      (name: value:
+      (self.callPackage value extraArgs)
+      )
+      (
+        lib.attrsets.mapAttrs
+          (name: _:
+          src + "/${name}"
+          )
+          (
+            lib.attrsets.filterAttrs
+              (_: type:
+              type == "directory"
+              )
+              (
+                builtins.readDir src
+              )
+          )
+      )
+  )
+  )
+) [ # Remove the scope vars that break packages
+  "callPackage"
+  "newScope"
+  "overrideScope"
+  "packages"
+]
