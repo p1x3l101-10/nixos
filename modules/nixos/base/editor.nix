05ed0ce4483af1abda3ec1938465271d56dcd54f
Pixel <scott.blatt.0b10@gmail.com>
2025-07-17 08:22:46 -0500

    flesh out vim

--- modules/nixos/base/editor.nix
+++ modules/nixos/base/editor.nix
@@ -1,8 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }: # Lib not needed usually, but is very nice for fakeHash later
 
 {
   programs.vim = {
     enable = true;
     defaultEditor = true;
+    package = (pkgs.vim-full.override { }).customize {
+      name = "vim-with-plugins";
+      vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+        start = [];
+        opt = [];
+      };
+      vimrcConfig.customRC = '''';
+    };
   };
 }
