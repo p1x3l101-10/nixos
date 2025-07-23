964112fce09623d31d4a002fe3a017ab380adcce
Pixel <scott.blatt.0b10@gmail.com>
2025-07-23 16:03:07 -0500

    steam env

--- modules/nixos/desktop/games/steam.nix
+++ modules/nixos/desktop/games/steam.nix
@@ -17,6 +17,10 @@
       adwaita-icon-theme
       morewaita-icon-theme
     ];
+    extraEnv = {
+      STEAM_LAUNCH_WRAPPER_SCOPE = "1";
+      PRESSURE_VESSEL_SYSTEMD_SCOPE = "1";
+    };
   };
   programs.alvr = {
     enable = true;
