d7b836be6812bbefb0218d45a782a51f6abff5eb
Pixel <scott.blatt.0b10@gmail.com>
2025-07-30 21:08:48 -0500

    hyprland

--- modules/nixos/base/cachix.nix
+++ modules/nixos/base/cachix.nix
@@ -10,5 +10,9 @@
       substituters = [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     }
+    {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    }
   ];
 }
