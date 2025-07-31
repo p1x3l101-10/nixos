c953263b25e0ac56df3173d4fd860a5d602cda59
Pixel <scott.blatt.0b10@gmail.com>
2025-07-30 19:41:02 -0500

    brought back wrong package

--- modules/nixos/desktop/games/minecraft.nix
+++
@@ -1,7 +0,0 @@
-{ inputs, pkgs, ... }:
-
-{
-  environment.systemPackages = [
-    inputs.fjordlauncher.packages."${pkgs.system}".fjordlauncher
-  ];
-}
---
+++ modules/nixos/desktop/games/steam.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+{
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam.override {
+      extraEnv = {
+        STEAM_LAUNCH_WRAPPER_SCOPE = "1";
+        PRESSURE_VESSEL_SYSTEMD_SCOPE = "1";
+      };
+    };
+    protontricks.enable = true;
+    localNetworkGameTransfers.openFirewall = true;
+    remotePlay.openFirewall = true;
+    gamescopeSession.enable = true;
+    extest.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+      steam-play-none
+    ];
+    extraPackages = with pkgs; [
+      gamescope
+      adwaita-icon-theme
+      morewaita-icon-theme
+    ];
+  };
+  programs.alvr = {
+    enable = true;
+    openFirewall = true;
+  };
+  environment.systemPackages = with pkgs; [
+    r2modman
+  ];
+  system.allowedUnfree.packages = [
+    "steam"
+    "steam-unwrapped"
+  ];
+}
