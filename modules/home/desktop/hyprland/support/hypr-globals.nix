136271e1a8981dfc378326bcb343ace7b80a98ef
Pixel <scott.blatt.0b10@gmail.com>
2025-07-31 18:24:09 -0500

    app macro

--- modules/home/desktop/hyprland/support/hypr-globals.nix
+++ modules/home/desktop/hyprland/support/hypr-globals.nix
@@ -1,13 +1,17 @@
-pkgs: lib: lib.fix(self: {
+pkgs: lib:
+let
+  app = path: text: (builtins.toString pkgs.writeShellApplication { inherit text; name = "app"; runtimeInputs = path; });
+in
+lib.fix (self: {
   modiferKey = "SUPER";
-  terminal = (builtins.toString pkgs.writeShellApplication { name = "terminal"; runtimeInputs = with pkgs; [ kitty ]; text = ''
+  terminal = (app [ pkgs.kitty ] ''
     kitty
-  ''; });
-  fileManager = (builtins.toString pkgs.writeShellApplication { name = "fileManager"; runtimeInputs = with pkgs; [ dolphin ]; text = ''
+  '');
+  fileManager = (app [ pkgs.dolphin ] ''
     dolphin
-  ''; });
-  spotlight = (builtins.toString pkgs.writeShellApplication { name = "spotlight"; runtimeInputs = with pkgs; [ wofi ]; text = ''
+  '');
+  spotlight = (app [ pkgs.wofi ] ''
     wofi --show drun
-  ''; });
+  '');
   appLauncher = self.spotlight;
 })
