85865a9c77af22264a34173d212be41f6cbf8565
Pixel <scott.blatt.0b10@gmail.com>
2025-07-30 21:08:31 -0500

    make cachix easier to manage

--- modules/nixos/base/cachix.nix
+++ modules/nixos/base/cachix.nix
@@ -1,13 +1,14 @@
-{ ... }:
+{ lib, ... }:
+
 {
-  nix.settings = {
-    substituters = [
-      "https://nix-community.cachix.org"
-      "https://unmojang.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "unmojang.cachix.org-1:OfHnbBNduZ6Smx9oNbLFbYyvOWSoxb2uPcnXPj4EDQY="
-    ];
-  };
+  nix.settings = lib.internal.attrsets.mergeAttrs [
+    {
+      substituters = [ "https://unmojang.cachix.org" ];
+      trusted-public-keys = [ "unmojang.cachix.org-1:OfHnbBNduZ6Smx9oNbLFbYyvOWSoxb2uPcnXPj4EDQY=" ];
+    }
+    {
+      substituters = [ "https://nix-community.cachix.org" ];
+      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    }
+  ];
 }
