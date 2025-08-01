32723faae56dbd6962141e8c05376dbaa8c73253
Pixel <scott.blatt.0b10@gmail.com>
2025-08-01 17:54:16 -0500

    stylix cursor

--- modules/home/desktop/hyprland/cursor.nix
+++ modules/home/desktop/hyprland/cursor.nix
@@ -1,13 +1,11 @@
 { pkgs, ... }:
 
 {
-  home.pointerCursor = {
+  stylix.cursor = {
     enable = true;
     name = "rose-pine-hyprcursor";
     size = 24;
     package = pkgs.rose-pine-hyprcursor;
-    gtk.enable = true;
-    hyprcursor.enable = true;
-    x11.enable = true;
   };
+  home.pointerCursor.hyprcursor.enable = true;
 }
