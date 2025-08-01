a17a3f52b2e25fd991eeced2f691429e9e44e5dc
Pixel <scott.blatt.0b10@gmail.com>
2025-07-31 23:24:50 -0500

    fix stylix

--- modules/home/stylix/ashell.nix
+++ modules/home/stylix/ashell.nix
@@ -1,23 +1,20 @@
-{ config, lib, mkTarget, ... }:
+{ config, lib, ... }:
 
-mkTarget {
-  name = "ashell";
-  humanName = "ashell";
-  configElements = [
-    ({ colors }: {
-      programs.ashell.settings.appearance = with colors; {
-        background_color = base00;
-        primary_color = base0E;
-        secondary_color = base03;
-        success_color = base03;
-        danger_color = base08;
-        text_color = base05;
-        workspace_colors = [ base0D ];
-        special_workspace_colors = [ base0D ];
-      };
-    })
-    ({ fonts }: {
-      programs.ashell.settings.appearance.font_name = fonts.sansSerif;
-    })
-  ];
+{
+  options.stylix.targets.ashell.enable = config.lib.stylix.mkEnableTarget "ashell" true;
+  config = lib.mkIf (config.stylix.targets.ashell.enable == true) (let
+      inherit (config.lib.stylix) colors fonts;
+    in {
+    programs.ashell.settings.appearance = with colors; {
+      font_name = fonts.sansSerif;
+      background_color = base00;
+      primary_color = base0E;
+      secondary_color = base03;
+      success_color = base03;
+      danger_color = base08;
+      text_color = base05;
+      workspace_colors = [ base0D ];
+      special_workspace_colors = [ base0D ];
+    };
+  });
 }
