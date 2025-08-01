92b2e8bd6465bb7e94b5a436f008fd3643e77f12
Pixel <scott.blatt.0b10@gmail.com>
2025-08-01 18:40:23 -0500

    merge hyprcursor and standard varients

--- modules/home/desktop/hyprland/cursor.nix
+++ modules/home/desktop/hyprland/cursor.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   stylix.cursor = {
-    name = "rose-pine-hyprcursor";
+    name = "rose-pine-cursor";
     size = 24;
-    package = pkgs.rose-pine-hyprcursor;
+    package = pkgs.buildEnv {
+      name = "rose-pine-cursor-merged";
+      paths = with pkgs; [
+        rose-pine-hyprcursor
+        rose-pine-cursor
+      ];
+    };
   };
+  home.sessionVariables.HYPRCURSOR_THEME = lib.mkForce "rose-pine-hyprcursor";
   home.pointerCursor.hyprcursor.enable = true;
 }
