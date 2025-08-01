f27c1bb933b47223156b18b0080beaa97b36b137
Pixel <scott.blatt.0b10@gmail.com>
2025-08-01 18:34:18 -0500

    override vesktop to use wayland

--- modules/home/desktop/apps/programs/discord.nix
+++ modules/home/desktop/apps/programs/discord.nix
@@ -1,8 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.vesktop = {
     enable = true;
+    package = pkgs.vesktop.overrideAttrs (oldAttrs: {
+      desktopItems = [
+        ((builtins.elemAt oldAttrs.desktopItems 0).override {
+          exec = "vesktop --ozone-platform-hint=auto %U";
+        })
+      ];
+    });
     vencord = {
       useSystem = true;
     };
