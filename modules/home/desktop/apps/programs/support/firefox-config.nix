0eded47efd5499261b309b4de9f971668af5ef77
Pixel <scott.blatt.0b10@gmail.com>
2025-08-02 17:52:30 -0500

    import some config values

--- modules/home/desktop/apps/programs/support/firefox-config.nix
+++ modules/home/desktop/apps/programs/support/firefox-config.nix
@@ -3,5 +3,11 @@
   browser = {
 	  search.suggest.enabled = true;
 	  startup.page = 0;
+	  firefox-view.feature-tour = builtins.toJSON {
+	    message = "FIREFOX_VIEW_FEATURE_TOUR";
+	    screen = "";
+	    complete = true;
+	  };
   };
+  fission.autostart = true;
 }
