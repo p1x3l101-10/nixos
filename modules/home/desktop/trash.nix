afb95029f6aaab2f6a2e7d9a96038ab42254b74d
Pixel <scott.blatt.0b10@gmail.com>
2025-08-03 16:12:21 -0500

    fix trash

--- modules/home/desktop/trash.nix
+++ modules/home/desktop/trash.nix
@@ -6,9 +6,9 @@
     fzf
   ];
   home.shellAliases = {
-    rm = "trashy put";
+    rm = "trash put";
     rm-force = "/run/current-system/sw/bin/rm";
-    rm-list = "trashy list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy restore --match=exact --force";
-    rm-empty = "trashy list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy empty --match=exact --force";
+    rm-list = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy restore --match=exact --force";
+    rm-empty = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy empty --match=exact --force";
   };
 }
