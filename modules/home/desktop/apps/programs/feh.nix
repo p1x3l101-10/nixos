f3fcea70117538c7dbff9daea7baa370250785ff
Pixel <scott.blatt.0b10@gmail.com>
2025-08-02 19:28:41 -0500

    custom package overrides for feh

--- modules/home/desktop/apps/programs/feh.nix
+++ modules/home/desktop/apps/programs/feh.nix
@@ -1,8 +1,22 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.feh = {
     enable = true;
+    package = pkgs.feh.overrideAttrs (oldAttrs: {
+      # Enable auto convert
+      buildInputs = oldAttrs.buildInputs ++ (with pkgs; [ file ]);
+      makeFlags = oldAttrs.makeFlags ++ [ "magic=1" ];
+      postInstall = ''
+        wrapProgram "$out/bin/feh" --prefix PATH : "${
+          lib.makeBinPath (with pkgs; [
+            libjpeg
+            jpegexiforient
+          ])
+        }" \
+                               --add-flags '--theme=feh --scale-down'
+      '';
+    });
   };
 }
 
