7bfa5008a7cee9f69204206f6589b46b1b450415
Pixel <scott.blatt.0b10@gmail.com>
2025-08-04 16:31:33 -0500

    allow getting local themes

--- modules/home/desktop/hyprland/shell.nix
+++ modules/home/desktop/hyprland/shell.nix
@@ -12,6 +12,7 @@
     };
     Service = {
       ExecStart = "${pkgs.quickshell}/bin/quickshell";
+      Environment = "QML_XHR_ALLOW_FILE_READ=1";
     };
     Install.WantedBy = [ "hyprland-session.target" ];
   };
