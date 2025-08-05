33569afd43b1e08ab433b5d0d27ed54821467411
Pixel <scott.blatt.0b10@gmail.com>
2025-08-05 17:46:33 -0500

    sessionvars

--- modules/home/desktop/apps/programs/app2unit.nix
+++ modules/home/desktop/apps/programs/app2unit.nix
@@ -4,5 +4,10 @@ let
   inherit (ext) inputs system;
   app2unit = inputs.app2unit.packages.${system}.app2unit;
 in {
-  home.packages = [ app2unit ];
+  home = {
+    packages = [ app2unit ];
+    sessionVariables = {
+      APP2UNIT_SLICES = "a=app-graphical.slice b=background-graphical.slice s=session-graphical.slice";
+    };
+  };
 }
