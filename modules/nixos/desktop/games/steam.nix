27f3f5d9dfe455dc402a7fd8a345ac98d1055db2
Pixel <scott.blatt.0b10@gmail.com>
2025-08-06 15:08:49 -0500

    vulkan pkgs

--- modules/nixos/desktop/games/steam.nix
+++ modules/nixos/desktop/games/steam.nix
@@ -39,7 +39,20 @@
     "steam-unwrapped"
   ];
   hardware.graphics = {
+    enable = true;
     enable32Bit = true;
     package32 = pkgs.pkgsi686Linux.mesa;
+    extraPackages = with pkgs; [
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-extension-layer
+      vulkan-tools
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-extension-layer
+      vulkan-tools
+    ];
   };
 }
