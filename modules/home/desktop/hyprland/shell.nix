d2d8a53cbd8aff154bc545d4840677569c785568
Pixel <scott.blatt.0b10@gmail.com>
2025-08-07 16:55:14 -0500

    shell

--- modules/home/desktop/hyprland/shell.nix
+++ modules/home/desktop/hyprland/shell.nix
@@ -21,6 +21,6 @@
     Install.WantedBy = [ "hyprland-session.target" ];
   };
   systemd.user.tmpfiles.rules = [
-    "L /home/pixel/.config/quickshell - - - - ${osConfig.environment.etc."nixos".source}/modules/home/desktop/hyprland/shell"
+    "L /home/pixel/.config/quickshell - - - - ${osConfig.environment.etc."nixos".source}/modules/home/desktop/hyprland/shell/kurukurubar"
   ];
 }
