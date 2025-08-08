26e1a7872e022681eeb25d4826d36d349cbef6ca
Pixel <scott.blatt.0b10@gmail.com>
2025-08-08 12:33:45 -0500

    style

--- modules/home/desktop/apps/programs/thunderbird.nix
+++ modules/home/desktop/apps/programs/thunderbird.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
   programs.thunderbird = {
@@ -7,7 +7,17 @@
     profiles.default = {
       isDefault = true;
       extensions = [];
-      settings = lib.internal.attrsets.compressAttrs "." (import ./support/firefox-config.nix);
+      settings = lib.internal.attrsets.compressAttrs "." (lib.internal.attrsets.mergeAttrs [(import ./support/firefox-config.nix) {
+        font.name = {
+          monospace.x-western = config.stylix.fonts.monospace.name;
+          sans-serif.x-western = config.stylix.fonts.sansSerif.name;
+          serif.x-western = config.stylix.fonts.serif.name;
+        };
+        toolkit.legacyUserProfileCustomizations.stylesheets = true;
+        svg.context-properties.content.enabled = true;
+      }]);
+      userChrome = builtins.readFile (config.programs.zen-browser.profiles."mlls93c4.Default (beta)".userChrome);
+      userContent = builtins.readFile (config.programs.zen-browser.profiles."mlls93c4.Default (beta)".userContent);
     };
   };
   home.sessionVariables.MOZ_ENABLE_WAYLAND = 1;
