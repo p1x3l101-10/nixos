29c15a5dbee8d7de0fe4bf0b531686bda5f5f54c
Pixel <scott.blatt.0b10@gmail.com>
2025-08-07 19:19:49 -0500

    change theme

--- modules/nixos/desktop/stylix.nix
+++ modules/nixos/desktop/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/uwunicorn.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/helios.yaml";
     image = with config.lib.stylix.colors.withHashtag;
       pkgs.runCommand "cat.png" {} ''
         pastel=${pkgs.pastel}/bin/pastel
