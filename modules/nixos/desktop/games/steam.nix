2743aaccfb0a98115801ea1e2feb5f809f0b4a68
Pixel <scott.blatt.0b10@gmail.com>
2025-08-09 17:00:35 -0500

    steam hardwarre

--- modules/nixos/desktop/games/steam.nix
+++ modules/nixos/desktop/games/steam.nix
@@ -40,21 +40,24 @@
     "steam"
     "steam-unwrapped"
   ];
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    package32 = pkgs.pkgsi686Linux.mesa;
-    extraPackages = with pkgs; [
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
-      vulkan-tools
-    ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
-      vulkan-tools
-    ];
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      package32 = pkgs.pkgsi686Linux.mesa;
+      extraPackages = with pkgs; [
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-extension-layer
+        vulkan-tools
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-extension-layer
+        vulkan-tools
+      ];
+    };
+    steam-hardware.enable = true;
   };
 }
