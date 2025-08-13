ec5254c6385efdca1789379532ccbaac797802d6
Pixel <scott.blatt.0b10@gmail.com>
2025-08-12 21:27:30 -0500

    add DF

---
+++ modules/home/desktop/apps/games/dwarf-fortress.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs.dwarf-fortress-packages; [
+    (dwarf-fortress-full.override  {})
+  ];
+  home.allowedUnfree.packages = [
+    "dwarf-fortress"
+    "phoebus-theme"
+  ];
+}
