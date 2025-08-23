5bc3d27ff1006aa68d9f0b9ebc8d9c06f24b35ad
Pixel <scott.blatt.0b10@gmail.com>
2025-08-23 00:24:13 -0500

    install flatpak

--- modules/home/desktop/apps/programs/newelle.nix
+++ modules/home/desktop/apps/programs/newelle.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  home.packages = [ pkgs.internal.nyarchAssistant ];
+  services.flatpak.packages = [
+    (lib.fix (self: {
+      appId = "moe.nyarchlinux.assistant";
+      sha256 = lib.fakeHash;
+      bundle = "${pkgs.fetchurl {
+        url = "https://github.com/NyarchLinux/NyarchAssistant/releases/download/1.0.0/nyarchassistant.flatpak";
+        inherit (self) sha256;
+      }}";
+    }))
+  ];
+  services.flatpak.overrides."moe.nyarchlinux.assistant" = {
+    Context.filesystems = [ "home" ];
+  };
 }
