76685e4bf8715c0a462de20a0d1ed2487f4e3da9
Pixel <scott.blatt.0b10@gmail.com>
2025-08-23 00:30:39 -0500

    update hash

--- modules/home/desktop/apps/programs/newelle.nix
+++ modules/home/desktop/apps/programs/newelle.nix
@@ -4,7 +4,7 @@
   services.flatpak.packages = [
     (lib.fix (self: {
       appId = "moe.nyarchlinux.assistant";
-      sha256 = lib.fakeHash;
+      sha256 = "sha256-7Bidqdy8TGsgliYFRY7oQ4JLGnWSQGTuyktMhe+ndC4=";
       bundle = "${pkgs.fetchurl {
         url = "https://github.com/NyarchLinux/NyarchAssistant/releases/download/1.0.0/nyarchassistant.flatpak";
         inherit (self) sha256;
