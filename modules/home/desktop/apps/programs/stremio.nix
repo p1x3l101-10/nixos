6de1ac522a77350daa20fcc1394a20bbc19ee339
Pixel <scott.blatt.0b10@gmail.com>
2025-09-01 14:33:51 -0500

    use updated stremio

--- modules/home/desktop/apps/programs/stremio.nix
+++ modules/home/desktop/apps/programs/stremio.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 
 {
+  /*
   home.packages = [ pkgs.stremio ];
   home.allowedUnfree.packages = [
     "stremio-shell"
     "stremio-server"
   ];
+  */
+  home.packages = [ pkgs.internal.stremio-linux-shell ];
 }
