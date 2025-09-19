d2335eccca5cf1f5b5124197c7c47904ea34fece
Pixel <scott.blatt.0b10@gmail.com>
2025-09-19 18:10:58 -0500

    fix bad cache

--- modules/nixos/base/content-addressed.nix
+++ modules/nixos/base/content-addressed.nix
@@ -2,7 +2,7 @@
 {
   nix.settings = {
     experimental-features = [ "ca-derivations" ];
-    substituters = [ "https://cache.ngi0.nixos.org" ];
-    trusted-public-keys = [ "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA=" ];
+    #substituters = [ "https://cache.ngi0.nixos.org" ];
+    #trusted-public-keys = [ "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA=" ];
   };
 }
