494d715a23cfc23c0f5317ae780cb5637ff8e668
Pixel <scott.blatt.0b10@gmail.com>
2025-10-04 13:15:23 -0500

    fix vCamera

--- modules/nixos/desktop/vCamera.nix
+++ modules/nixos/desktop/vCamera.nix
@@ -1,10 +1,18 @@
 { config, ... }:
 {
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  boot = {
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
+    kernelModules = [
+      "v4l2loopback"
+    ];
+  };
+  services.udev.extraRules = ''
+    SUBSYSTEM=="video4linux", ENV{V4L2_ALLOW_VIRTUAL_CAMERA}="1"
   '';
   security.polkit.enable = true;
 }
