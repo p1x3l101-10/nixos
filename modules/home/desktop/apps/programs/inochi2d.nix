a6391f5cdca88bf294eada06b704cac4968d5093
Pixel <scott.blatt.0b10@gmail.com>
2025-10-10 15:07:21 -0500

    temp disable the packages until they are unborked

--- modules/home/desktop/apps/programs/inochi2d.nix
+++ modules/home/desktop/apps/programs/inochi2d.nix
@@ -1,14 +1,8 @@
 { pkgs, ... }:
 
-let
-  cmakeOverride = pkg: (pkg.overrideAttrs (oldAttrs: {
-      cmakeFlags = oldAttrs.cmakeFlags ++ [
-        "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
-      ];
-  }));
-in {
+{
   home.packages = with pkgs; [
-    (cmakeOverride inochi-session)
-    (cmakeOverride inochi-creator)
+    #inochi-session
+    #inochi-creator
   ];
 }
