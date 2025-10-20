fa1a1543c18608751b5d3fd744df045b3da6fe66
Pixel <scott.blatt.0b10@gmail.com>
2025-10-19 21:04:21 -0500

    basic vesktop settings

--- modules/home/desktop/apps/programs/discord.nix
+++ modules/home/desktop/apps/programs/discord.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 
-{
+let
+  enabledPlugins = list: (
+    builtins.listToAttrs (builtins.map (x:
+      {
+        name = x;
+        value = { enabled = true; };
+      }
+    ) list)
+  );
+in {
   programs.vesktop = {
     enable = true;
     package = pkgs.vesktop.overrideAttrs (oldAttrs: {
@@ -12,6 +21,28 @@
     });
     vencord = {
       useSystem = true;
+      settings = {
+        autoUpdate = false;
+        autoUpdateNotification = false;
+        useQuickCss = false;
+        themeLinks = [];
+        enabledThemes = [];
+        enableReactDevtools = false;
+        frameless = true;
+        transparent = false;
+        winCtrlQ = false;
+        disableMinSize = false;
+        winNativeTitleBar = false;
+        plugins = enabledPlugins [];
+        notifications = {
+          timeout = 5000;
+          position = "bottom-right";
+          useNative = "not-focused";
+          logLimit = 50;
+        };
+        # No cloud needed
+        #cloud = {};
+      };
     };
   };
 }
