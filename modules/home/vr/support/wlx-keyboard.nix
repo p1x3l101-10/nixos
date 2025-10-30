b9b372b06c151cc17662b9f98374c885fac1edbd
Pixel <scott.blatt.0b10@gmail.com>
2025-10-29 19:34:43 -0500

    fix null chars

--- modules/home/vr/support/wlx-keyboard.nix
+++ modules/home/vr/support/wlx-keyboard.nix
@@ -10,12 +10,12 @@
     [1.25  1.25  1.25  6.25  1.25  1.25  1.25  1.25  0.5  1  1  1  0.5     2     1  1]
   ];
   main_layout = [
-    ["Escape" "~" "F1"  "F2"  "F3"  "F4" "~" "F5"  "F6"  "F7"  "F8" "~" "F9"  "F10"  "F11"  "F12" "~" "Print"  "Scroll"  "Pause" "~" "COPY"  "PASTE" "~" "KILL"]
-    ["Oem3"  "N1"  "N2"  "N3"  "N4"  "N5"  "N6"  "N7"  "N8"  "N9"  "N0"  "Minus"  "Plus"  "BackSpace" "~" "Insert"  "Home"  "Prior" "~" "NumLock"  "KP_Divide"  "KP_Multiply"  "KP_Subtract"]
-    ["Tab"  "Q"  "W"  "E"  "R"  "T"  "Y"  "U"  "I"  "O"  "P"  "Oem4"  "Oem6"  "Oem5" "~" "Delete"  "End"  "Next" "~" "KP_7"  "KP_8"  "KP_9"  "KP_Add"]
-    ["XF86Favorites"  "A"  "S"  "D"  "F"  "G"  "H"  "J"  "K"  "L"  "Oem1"  "Oem7"  "Return" "~" "KP_4"  "KP_5"  "KP_6" "~"]
-    ["LShift"  "Oem102"  "Z"  "X"  "C"  "V"  "B"  "N"  "M"  "Comma"  "Period"  "Oem2"  "RShift" "~" "Up" "~" "KP_1"  "KP_2"  "KP_3"  "KP_Enter"]
-    ["LCtrl"  "LSuper"  "LAlt"  "Space"  "Meta"  "RSuper"  "Menu"  "RCtrl" "~" "Left"  "Down"  "Right" "~" "KP_0"  "KP_Decimal" "~"]
+    ["Escape" null "F1"  "F2"  "F3"  "F4" null "F5"  "F6"  "F7"  "F8" null "F9"  "F10"  "F11"  "F12" null "Print"  "Scroll"  "Pause" null "COPY"  "PASTE" null "KILL"]
+    ["Oem3"  "N1"  "N2"  "N3"  "N4"  "N5"  "N6"  "N7"  "N8"  "N9"  "N0"  "Minus"  "Plus"  "BackSpace" null "Insert"  "Home"  "Prior" null "NumLock"  "KP_Divide"  "KP_Multiply"  "KP_Subtract"]
+    ["Tab"  "Q"  "W"  "E"  "R"  "T"  "Y"  "U"  "I"  "O"  "P"  "Oem4"  "Oem6"  "Oem5" null "Delete"  "End"  "Next" null "KP_7"  "KP_8"  "KP_9"  "KP_Add"]
+    ["XF86Favorites"  "A"  "S"  "D"  "F"  "G"  "H"  "J"  "K"  "L"  "Oem1"  "Oem7"  "Return" null "KP_4"  "KP_5"  "KP_6" null]
+    ["LShift"  "Oem102"  "Z"  "X"  "C"  "V"  "B"  "N"  "M"  "Comma"  "Period"  "Oem2"  "RShift" null "Up" null "KP_1"  "KP_2"  "KP_3"  "KP_Enter"]
+    ["LCtrl"  "LSuper"  "LAlt"  "Space"  "Meta"  "RSuper"  "Menu"  "RCtrl" null "Left"  "Down"  "Right" null "KP_0"  "KP_Decimal" null]
   ];
   alt_modifier = "None";
   exec_commands = {
