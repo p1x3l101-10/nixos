d955a3e9c52962034200c6d5490f0792cf05a084
Pixel <scott.blatt.0b10@gmail.com>
2025-10-29 19:41:42 -0500

    add more settings

--- modules/home/vr/overlay.nix
+++ modules/home/vr/overlay.nix
@@ -6,5 +6,6 @@
     "wlxoverlay/settings.yaml".source = (pkgs.formats.yaml { }).generate "settings.yaml" (import ./support/wlx-settings.nix);
     "wlxoverlay/watch.yaml".source = (pkgs.formats.yaml { }).generate "watch.yaml" (import ./support/wlx-watch.nix);
     "wlxoverlay/wayvr.conf.d/dashboard.yaml".source = (pkgs.formats.yaml { }).generate "dashboard.yaml" (import ./support/wayvr-dash.nix { mainProg = "${pkgs.wayvr-dashboard}/bin/wayvr-dashboard"; });
+    "wlxoverlay/wayvr.conf.d/settings.yaml".source = (pkgs.formats.yaml { }).generate "dashboard.yaml" (import ./support/wayvr-settings.nix);
   };
 }
---
+++ modules/home/vr/support/wayvr-settings.nix
@@ -0,0 +1,10 @@
+{
+  version = 1;
+  displays = {
+    disp1 = {
+      width = 640;
+      height = 480;
+      primary = true;
+    };
+  };
+}
