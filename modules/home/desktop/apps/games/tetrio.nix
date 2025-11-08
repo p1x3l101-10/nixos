6463530fea5b1620b93b7271afc44bb1e34b4354
Pixel <scott.blatt.0b10@gmail.com>
2025-11-07 19:35:36 -0600

    try to fix it

--- modules/home/desktop/apps/games/tetrio.nix
+++ modules/home/desktop/apps/games/tetrio.nix
@@ -1,16 +1,16 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = [
-    ((pkgs.tetrio-desktop.override { withTetrioPlus = true; }).overrideAttrs (oldAttrs: (lib.fixedPoints.fix (finalAttrs: {
+    ((pkgs.tetrio-desktop.override { withTetrioPlus = false; }).overrideAttrs (oldAttrs: {
       version = "10";
 
       src = pkgs.fetchzip {
-        url = "https://tetr.io/about/desktop/builds/${finalAttrs.version}/TETR.IO%20Setup.deb";
+        url = "https://tetr.io/about/desktop/builds/10/TETR.IO%20Setup.deb";
         hash = "sha256-2FtFCajNEj7O8DGangDecs2yeKbufYLx1aZb3ShnYvw=";
         nativeBuildInputs = [ pkgs.dpkg ];
       };
-    }))))
+    }))
   ];
   home.allowedUnfree.packages = [
     "tetrio-desktop"
