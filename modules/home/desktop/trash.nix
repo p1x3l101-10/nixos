9db03fcae1f375bb2a6b79812b16ad12a0a67b8c
Pixel <scott.blatt.0b10@gmail.com>
2025-11-19 10:48:46 -0600

    fix trash aliases

--- modules/home/desktop/trash.nix
+++ modules/home/desktop/trash.nix
@@ -6,9 +6,7 @@
     fzf
   ];
   home.shellAliases = {
-    rm = "trash put";
-    rm-force = "/run/current-system/sw/bin/rm";
-    rm-list = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy restore --match=exact --force";
-    rm-empty = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy empty --match=exact --force";
+    trash-list = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy restore --match=exact --force";
+    trash-empty = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trashy empty --match=exact --force";
   };
 }
