131bf178054cbe05db534e228de5c98a91b16721
Pixel <scott.blatt.0b10@gmail.com>
2025-11-26 16:15:22 -0600

    swap back

--- modules/home/desktop/apps/programs/stremio.nix
+++ modules/home/desktop/apps/programs/stremio.nix
@@ -1,16 +1,16 @@
 { pkgs, ... }:
 
 {
-  /*
   home.packages = [ pkgs.stremio ];
   home.allowedUnfree.packages = [
     "stremio-shell"
     "stremio-server"
   ];
-  */
+  /*
   home.packages = [ pkgs.internal.stremio-linux-shell ];
   home.allowedUnfree.packages = [
     "stremio-linux-shell"
     "stremio-server"
   ];
+  */
 }
