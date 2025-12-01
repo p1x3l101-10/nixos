2267820caa2b4b3c0341f532daa59c44e7fc2a98
Pixel <scott.blatt.0b10@gmail.com>
2025-12-01 13:22:59 -0600

    use HM provided quickshell config

--- modules/home/desktop/shell/configLink.nix
+++
@@ -1,7 +0,0 @@
-{ osConfig, ... }:
-
-{
-  systemd.user.tmpfiles.rules = [
-    "L /home/pixel/.config/quickshell - - - - ${osConfig.environment.etc."nixos".source}/modules/home/desktop/shell/config"
-  ];
-}
--- modules/home/desktop/shell/default.nix
+++ modules/home/desktop/shell/default.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ pkgs, osConfig, ... }:
 
 {
-  imports = (lib.internal.confTemplates.importList ./.);
+  programs.quickshell = {
+    enable = true;
+    systemd = {
+      enable = true;
+      target = "hyprland-session.target";
+    };
+  };
+  home.packages = with pkgs; [
+    quickshell
+    material-symbols
+  ];
+  systemd.user.tmpfiles.rules = [
+    "L /home/pixel/.config/quickshell - - - - ${osConfig.environment.etc."nixos".source}/modules/home/desktop/shell/config"
+  ];
 }
