2028155374df7cc83c0183c65eb07a090d69a48f
Pixel <scott.blatt.0b10@gmail.com>
2025-12-11 09:02:11 -0600

    swap import format

--- modules/nixos/base/default.nix
+++ modules/nixos/base/default.nix
@@ -1,20 +1,5 @@
 { lib, inputs, ... }:
 {
-  imports = [
-    ./autoUpgrade.nix
-    ./base.nix
-    ./bash.nix
-    ./cachix.nix
-    ./cache.nix
-    ./content-addressed.nix
-    ./editor.nix
-    ./git.nix
-    ./hideGetty.nix
-    ./impermanence.nix
-    ./ipfs.nix
-    ./locale.nix
-    ./secureBoot.nix
-    ./ssh.nix
-  ];
+  imports = (lib.internal.confTemplates.importList ./.);
   nix.registry = lib.internal.confTemplates.registry inputs;
 }
--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -6,38 +6,6 @@
     };
   };
   imports = [
-    ./adb.nix
-    ./home-licences.nix
-    ./home.nix
-    ./ipfs.nix
-    ./kernel.nix
-    ./modpack-working.nix
-    ./impermanence.nix
-    ./nebula.nix
-    ./nix-exper.nix
-    ./nix-ld.nix
-    ./nix.nix
-    ./nss.nix
-    #./ollama.nix
-    ./plymouth.nix
-    ./powerManagement.nix
-    ./printing.nix
-    ./samba.nix
-    ./silence.nix
-    ./sound.nix
-    ./stylix.nix
-    ./tablet.nix
-    ./vCamera.nix
-    #./vr-min.nix
-    ./vr.nix
-
-    ./desktop/flatpak.nix
-    #./desktop/fonts.nix
-    #./desktop/gnome.nix
-    ./desktop/hyprland.nix
-    ./desktop/syncthing.nix
-    ./desktop/waydroid.nix
-
-    ./games/steam.nix
-  ];
+    ./desktop
+  ] ++ (lib.internal.confTemplates.importList ./.);
 }
