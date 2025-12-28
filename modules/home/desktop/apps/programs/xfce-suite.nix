b5cba21f6852e6b8782f88adcf0ce5c4cf36aae8
Pixel <scott.blatt.0b10@gmail.com>
2025-12-27 20:51:24 -0600

    configure xarchiver

--- modules/home/desktop/apps/programs/xfce-suite.nix
+++ modules/home/desktop/apps/programs/xfce-suite.nix
@@ -1,22 +1,27 @@
 { lib, pkgs, ... }:
 
 {
-  programs.thunar = {
-    enable = true;
-    thumbnails = true;
-    config = {
-      actions = [
-        {
-          icon = "utilities-terminal";
-          name = "Open Terminal Here";
-          unique-id = "1754173888245658-1";
-          command = "exo-open --working-directory %f --launch TerminalEmulator";
-          description = "Example for a custom action";
-          patterns = [ "*" ];
-          startup-notify = true;
-          directories = true;
-        }
-      ];
+  programs = {
+    thunar = {
+      enable = true;
+      thumbnails = true;
+      config = {
+        actions = [
+          {
+            icon = "utilities-terminal";
+            name = "Open Terminal Here";
+            unique-id = "1754173888245658-1";
+            command = "exo-open --working-directory %f --launch TerminalEmulator";
+            description = "Example for a custom action";
+            patterns = [ "*" ];
+            startup-notify = true;
+            directories = true;
+          }
+        ];
+      };
+    };
+    xarchiver = {
+      enable = true;
     };
   };
   gtk.gtk3.bookmarks = (map
---
+++ modules/home/module/xarchiver.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.programs.xarchiver;
+  inherit (lib) mkOption types mkIf mkEnableOption;
+  mkInt = default: mkOption { type = types.int; inherit default; };
+  mkBool = default: mkOption { type = types.bool; inherit default; };
+  mkStr = default: mkOption { type = types.str; inherit default; };
+  mkPath = default: mkOption { type = types.path; inherit default; };
+in
+{
+  options.programs.xarchiver = {
+    enable = mkEnableOption "xarchiver";
+    config = {
+      preferred_format = mkInt 0;
+      prefer_unzip = mkBool true;
+      confirm_deletion = mkBool true;
+      sort_filename_content = mkBool false;
+      advanced_isearch = mkBool true;
+      store_output = mkBool false;
+      icon_size = mkInt 2;
+      show_archive_comment = mkBool false;
+      show_sidebar = mkInt true;
+      show_location_bar = true;
+      show_toolbar = true;
+      preferred_custom_cmd = mkStr "";
+      preferred_temp_dir = mkPath "/tmp";
+      preferred_extract_dir = mkPath "/tmp";
+      allow_sub_dir = mkInt 0;
+      extended_dnd = mkInt 1;
+      ensure_directory = mkBool true;
+      overwrite = mkBool false;
+      full_path = mkInt 2;
+      touch = mkBool false;
+      fresh = mkBool false;
+      update = mkBool false;
+      store_path = mkBool false;
+      updadd = mkBool true;
+      freshen = mkInt false;
+      recurse = mkBool true;
+      solid_archive = mkBool false;
+      remove_files = mkBool false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.xarchiver ];
+
+    xdg.configFile = {
+      "xarchiver/xarchiverrc".source = (pkgs.formats.ini {}).generate {
+        xarchiver = cfg.config;
+      };
+    };
+  };
+}
