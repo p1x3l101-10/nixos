87cff4485301c0104aa9fdd29d12c2eff87bd729
Pixel <scott.blatt.0b10@gmail.com>
2026-01-03 15:25:13 -0600

    vr compat option

--- modules/nixos/desktop/games/steam.nix
+++ modules/nixos/desktop/games/steam.nix
@@ -8,6 +8,7 @@
         STEAM_LAUNCH_WRAPPER_SCOPE = "1";
         PRESSURE_VESSEL_SYSTEMD_SCOPE = "1";
         DRI_PRIME = "0";
+        PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES = "1";
       };
     };
     protontricks.enable = true;
