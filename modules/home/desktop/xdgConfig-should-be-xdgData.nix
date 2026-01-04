7721eb9acc858249e93bafcebbd7889df696318b
Pixel <scott.blatt.0b10@gmail.com>
2026-01-04 15:04:55 -0600

    add links

---
+++ modules/home/desktop/xdgConfig-should-be-xdgData.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+
+{
+  systemd.tmpfiles.rules = (map
+    (x: "L ${config.xdg.configHome}/${x} - - - - ${config.xdg.dataHome}/${x}")
+    [ # Convert this list into a link from .config to .local/share
+      "steamtinkerlaunch"
+      "r2modmanPlus-local"
+      "vesktop/sessionData"
+      "r2modman"
+      "unity3d"
+    ]
+  );
+}
