2e38d286e764abc2884349df0f912cce01943dab
Pixel <scott.blatt.0b10@gmail.com>
2026-01-04 15:09:59 -0600

    fix option name

--- modules/home/desktop/xdgConfig-should-be-xdgData.nix
+++ modules/home/desktop/xdgConfig-should-be-xdgData.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 
 {
-  systemd.tmpfiles.rules = (map
+  systemd.user.tmpfiles.rules = (map
     (x: "L ${config.xdg.configHome}/${x} - - - - ${config.xdg.dataHome}/${x}")
     [ # Convert this list into a link from .config to .local/share
       "steamtinkerlaunch"
