1e8df619f8301cf863765fb796b7ed67a2806135
Pixel <scott.blatt.0b10@gmail.com>
2026-01-05 11:43:29 -0600

    close off

--- modules/home/desktop/xdg.nix
+++ modules/home/desktop/xdg.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }:
 
 let
-  hyprLib = lib.extend (final: prev: { hypr = import ./hyprland/support/hypr-lib.nix lib });
+  hyprLib = lib.extend (final: prev: { hypr = import ./hyprland/support/hypr-lib.nix lib; });
   hypr-globals = import ./hyprland/support/hypr-globals.nix pkgs hyprLib;
 in {
   xdg = {
