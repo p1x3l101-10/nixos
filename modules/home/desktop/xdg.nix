2babdd4bb41f6b81e9f8ae7f47a43833a54ae87d
Pixel <scott.blatt.0b10@gmail.com>
2026-01-05 11:39:12 -0600

    add hyprland terminal

--- modules/home/desktop/xdg.nix
+++ modules/home/desktop/xdg.nix
@@ -1,7 +1,17 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
-{
+let
+  hypr-globals = import ./hyprland/support/hypr-globals.nix pkgs lib;
+in {
   xdg = {
     userDirs.enable = true;
+    terminal-exec = {
+      enable = true;
+      settings = {
+        "start-hyprland:Hyprland" = [
+          (hypr-globals.apps.terminal.desktop)
+        ];
+      };
+    };
   };
 }
