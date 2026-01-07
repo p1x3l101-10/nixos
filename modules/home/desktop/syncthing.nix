edc61093137cb9d8f3904f2536b6bfb5898b02cb
Pixel <scott.blatt.0b10@gmail.com>
2026-01-06 19:54:46 -0600

    update syncthing

--- modules/home/desktop/syncthing.nix
+++ modules/home/desktop/syncthing.nix
@@ -3,6 +3,10 @@
 let
   inherit (osConfig.networking) hostName;
   normalDevices = {
+    deck = {
+      name = "Steam Deck";
+      id = "OFO7QBD-ERD47CK-YEMB4EA-2X7MPYB-SHRWJFK-YQ4GERM-56U2WI7-ITAMVA6";
+    };
     macbook = {
       name = "School MacBook Air";
       id = "3TR2UN5-A6VZAM7-TH6OGRS-B3Y2KJF-REGLPET-T72TP5Y-J63274F-74XX2QH";
@@ -10,6 +14,10 @@ let
     phone = {
       name = "Pixel 10 Pro";
       id = "GLIX2WJ-S3JYFO3-342GHQ5-6JN6EYD-HPK3DEG-ZW42UPV-FAFNNAM-5SDODQM";
+    };
+    quest = {
+      name = "Quest";
+      id = "K4GNSY7-XFID545-KA5SZRI-N32GQPQ-RP5NCAA-3G7L6PF-2QCS5GV-F6SNVA3";
     };
   };
   nixosDevices = {
@@ -34,11 +42,6 @@ let
     )
   );
   trimFirstTwo = str: (builtins.substring 2 (builtins.stringLength str - 2) str);
-  allPublicDevices = with devices; [
-    macbook
-    phone
-    pixels-pc
-  ];
   globalIgnorePatterns = ''
     (?d).DS_Store
     (?d).Spotlight-V100
@@ -48,55 +51,130 @@ let
   '';
   folders = {
     Audiobooks = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        macbook
+        phone
+        pixels-pc
+      ]);
       path = "~/Audiobooks";
       id = "4xkrg-i9dj1";
       ignore = '''';
     };
     Camera = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        quest
+        macbook
+        pixels-pc
+      ]);
       path = "~/Camera";
       id = "pixel_6_kjap-photos";
       ignore = '''';
     };
     "Default Folder" = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        quest
+        deck
+        macbook
+        pixels-pc
+      ]);
       path = "~/Sync";
       id = "sn1nr-pcw8m";
       ignore = '''';
     };
     Documents = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        quest
+        deck
+        macbook
+        pixels-pc
+      ]);
       path = "~/Documents";
       id = "7t10j-5frar";
       ignore = '''';
     };
     Downloads = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        quest
+        deck
+        macbook
+        pixels-pc
+      ]);
       path = "~/Downloads";
       id = "rsejh-jym7t";
       ignore = '''';
     };
+    Games = {
+      devices = getIds (with devices; [
+        deck
+        pixels-pc
+      ]);
+      path = "~/Games";
+      id = "45aik-9ajyr";
+      ignore = ''
+        /ROMs
+      '';
+    };
     Music = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        deck
+        macbook
+        pixels-pc
+      ]);
       path = "~/Music";
       id = "ly0bl-r2iqi";
       ignore = '''';
     };
     Oculus = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        quest
+        deck
+        macbook
+        pixels-pc
+      ]);
       path = "~/Oculus";
       id = "t5tkm-cvnpk";
       ignore = '''';
     };
+    "Phone Misc" = {
+      devices = getIds (with devices; [ 
+        pixels-pc 
+        phone 
+        quest
+      ])
+    };
     Pictures = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        deck
+        macbook
+        pixels-pc
+      ]);
       path = "~/Pictures";
       id = "omyo0-dp5p0";
       ignore = '''';
     };
+    RetroDECK = {
+      devices = getIds (with devices; [
+        deck
+        pixels-pc
+      ]);
+      path = "~/RetroDECK";
+      id = "retro-data";
+      ignore = '''';
+    };
     Videos = {
-      devices = getIds allPublicDevices;
+      devices = getIds (with devices; [
+        phone
+        deck
+        macbook
+        pixels-pc
+      ]);
       path = "~/Videos";
       id = "qoy8g-azv4o";
       ignore = '''';
