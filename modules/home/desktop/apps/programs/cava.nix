117a7562f788dc209d9857f69724bd24268daadb
Pixel <scott.blatt.0b10@gmail.com>
2026-01-11 21:59:54 -0600

    add configs

--- modules/home/desktop/apps/programs/cava.nix
+++ modules/home/desktop/apps/programs/cava.nix
@@ -8,6 +8,16 @@
         method = "pipewire";
         source = "auto";
       };
+      output = {
+        method = "noncurses";
+        orientation = "horizontal";
+        channels = "stereo";
+        disable_blanking = 1;
+      };
+      color = {
+        gradient = 1;
+        horizontal_gradient = 1;
+      };
     };
   };
 }
