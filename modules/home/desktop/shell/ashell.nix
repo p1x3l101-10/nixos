955e2515e5ee232c28228ab24076a27acbbe8a5c
Pixel <scott.blatt.0b10@gmail.com>
2026-01-15 21:47:38 -0600

    force color scheme

--- modules/home/desktop/shell/ashell.nix
+++ modules/home/desktop/shell/ashell.nix
@@ -1,12 +1,30 @@
-{ ... }:
+{ config, ... }:
 
-{
+let
+  inherit (config.lib.stylix) colors opacity;
+in {
   programs.ashell = {
     enable = true;
     systemd = {
       enable = true;
       target = "hyprland-session.target";
     };
-    settings = import ./ashell.config.nix;
+    settings = (import ./ashell.config.nix) // {
+      appearance = with colors.withHashtag; {
+        background_color = base00;
+        primary_color = base0D;
+        secondary_color = base01;
+        success_color = base0B;
+        danger_color = base09;
+        text_color = base05;
+
+        workspace_colors = [
+          base09
+          base0D
+        ];
+        opacity = opacity.desktop;
+        menu.opacity = opacity.desktop;
+      };
+    };
   };
 }
