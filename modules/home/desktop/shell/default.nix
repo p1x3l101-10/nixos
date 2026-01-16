26374c4cacec3c830254cb187c73e4db2478f43e
Pixel <scott.blatt.0b10@gmail.com>
2026-01-15 21:10:18 -0600

    shove quickshell to the side

--- modules/home/desktop/shell/default.nix
+++ modules/home/desktop/shell/default.nix
@@ -1,18 +1,5 @@
-{ pkgs, osConfig, ... }:
+{ ... }:
 
 {
-  programs.quickshell = {
-    enable = true;
-    systemd = {
-      enable = true;
-      target = "hyprland-session.target";
-    };
-  };
-  home.packages = with pkgs; [
-    quickshell
-    material-symbols
-  ];
-  systemd.user.tmpfiles.rules = [
-    "L /home/pixel/.config/quickshell - - - - ${osConfig.environment.etc."nixos".source}/modules/home/desktop/shell/config"
-  ];
+  imports = [ ./quickshell.nix ];
 }
