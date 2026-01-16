05431bf605748aa61254cf76c2a90dac15b9714d
Pixel <scott.blatt.0b10@gmail.com>
2026-01-15 21:38:16 -0600

    update ashell

--- modules/home/desktop/shell/ashell.config.nix
+++ modules/home/desktop/shell/ashell.config.nix
@@ -1,15 +1,16 @@
 {
   modules = {
     center = [
-      "Window Title"
+      "Clock"
     ];
     left = [
       "Workspaces"
     ];
     right = [
+      "MediaPlayer"
       "SystemInfo"
       [
-        "Clock"
+        "Tray"
         "Privacy"
         "Settings"
       ]
@@ -17,5 +18,56 @@
   };
   workspaces = {
     visibility_mode = "MonitorSpecific";
+  };
+  system_info = {
+    indicators = [
+      "CPU"
+      "Memory"
+      "MemorySwap"
+      { Disk = "/nix"; }
+      "DownloadSpeed"
+      "UploadSpeed"
+    ];
+    cpu = {
+      warn_threshold = 60;
+      alert_threshold = 80;
+    };
+    memory = {
+      warn_threshold = 80;
+      alert_threshold = 95;
+    };
+    disk = {
+      warn_threshold = 80;
+      alert_threshold = 90;
+    };
+  };
+  settings = {
+    battery_format = "IconAndPercentage";
+    peripheral_battery_format = "Icon";
+    peripheral_indicators = {
+      Specific = [
+        "Gamepad"
+        "Keyboard"
+      ];
+    };
+    indicators = [
+      "IdleInhibitor"
+      "PowerProfile"
+      "Audio"
+      "Bluetooth"
+      "Network"
+      "Vpn"
+      "Battery"
+    ];
+    shutdown_cmd = "systemctl shutdown";
+    suspend_cmd = "systemctl suspend";
+    reboot_cmd = "systemctl reboot";
+    logout_cmd = "loginctl kill-session $XDG_SESSION_ID";
+    audio_sources_more_cmd = "hyprpwcenter";
+    audio_sinks_more_cmd = "hyprpwcenter";
+    lock_cmd = "loginctl lock-session $XDG_SESSION_ID";
+  };
+  appearance = {
+    style = "Gradient";
   };
 }
