936b744beb74baf5d58dad530168ca93206decf3
Pixel <scott.blatt.0b10@gmail.com>
2026-01-15 21:55:46 -0600

    why did i ever doubt stylix

--- modules/home/desktop/shell/ashell.nix
+++ modules/home/desktop/shell/ashell.nix
@@ -1,31 +1,12 @@
-{ config, ... }:
+{ ... }:
 
-let
-  inherit (config.lib.stylix) colors;
-  inherit (config.stylix) opacity;
-in {
+{
   programs.ashell = {
     enable = true;
     systemd = {
       enable = true;
       target = "hyprland-session.target";
     };
-    settings = (import ./ashell.config.nix) // {
-      appearance = with colors.withHashtag; {
-        background_color = base00;
-        primary_color = base0D;
-        secondary_color = base01;
-        success_color = base0B;
-        danger_color = base09;
-        text_color = base05;
-
-        workspace_colors = [
-          base09
-          base0D
-        ];
-        opacity = opacity.desktop;
-        menu.opacity = opacity.desktop;
-      };
-    };
+    settings = import ./ashell.config.nix;
   };
 }
