3696656238d4126886e5fb85907d19c0a3e08bf6
Pixel <scott.blatt.0b10@gmail.com>
2026-01-17 15:21:07 -0600

    style it

--- modules/home/desktop/apps/games/millennium-steam.nix
+++ modules/home/desktop/apps/games/millennium-steam.nix
@@ -28,7 +28,25 @@ in {
             gamepage.biggerBanner = true;
           };
         };
-        colorOverrides = {};
+        colorOverrides = with colors; {
+          "--st-accent-1" = mkHexColor base0E;
+          "--st-accent-2" = mkHexColor base09;
+          "--st-color-1" = mkHexColor base01;
+          "--st-color-2" = mkHexColor base03;
+          "--st-color-3" = mkHexColor base02;
+          "--st-color-4" = mkHexColor base02;
+          "--st-color-5" = mkHexColor base03;
+          "--st-color-6" = mkHexColor base03;
+          "--st-background" = mkHexColor base00;
+          "--st-red" = mkHexColor red;
+          "--st-red-hover" = mkHexColor bright-red;
+          "--st-green" = mkHexColor green;
+          "--st-green-hover" = mkHexColor bright-green;
+          "--st-blue" = mkHexColor blue;
+          "--st-blue-hover" = mkHexColor bright-blue;
+          "--st-yellow" = mkHexColor yellow;
+          "--st-yellow-hover" = mkHexColor bright-yellow;
+        };
       };
       NEVKO-UI = {
         #enable = true;
