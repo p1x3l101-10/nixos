4de4a4b3b458ed8753e12db288b81c6f5a208304
Pixel <scott.blatt.0b10@gmail.com>
2026-01-16 23:43:28 -0600

    color steam

--- modules/home/desktop/apps/games/millennium-steam.nix
+++ modules/home/desktop/apps/games/millennium-steam.nix
@@ -1,10 +1,23 @@
-{ ... }:
+{ config, lib, ... }:
 
-{
+let
+  inherit (config.lib.stylix.colors) colors mkHexColor;
+in {
   programs.millenium-steam = {
     enable = true;
     themes.NEVKO-UI = {
       enable = true;
+      colorOverrides = with colors; {
+        "--custom-accent" = mkHexColor base0B;
+        "--green-color" = mkHexColor green;
+        "--idle-status" = mkHexColor base0D;
+        "--ingame-status" = mkHexColor base0A;
+        "--ingameidle-status" = mkHexColor base0C;
+        "--main-background" = mkHexColor base00;
+        "--offline-status" = mkHexColor base03;
+        "--online-status" = mkHexColor cyan;
+        "--purple-color" = mkHexColor purple;
+      };
     };
   };
 }
