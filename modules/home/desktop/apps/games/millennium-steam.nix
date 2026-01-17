9a60becac16043c21aae98ae3d57fb1298e21a27
Pixel <scott.blatt.0b10@gmail.com>
2026-01-17 14:53:32 -0600

    expand option

--- modules/home/desktop/apps/games/millennium-steam.nix
+++ modules/home/desktop/apps/games/millennium-steam.nix
@@ -14,21 +14,23 @@ in {
       };
       misc.hasShownWelcomeModal = true;
     };
-    themes.NEVKO-UI = {
-      enable = true;
-      settings = {
-        removeWindowButtons = "All";
-      };
-      colorOverrides = with colors; {
-        "--custom-accent" = mkHexColor base0B;
-        "--green-color" = mkHexColor green;
-        "--idle-status" = mkHexColor base0D;
-        "--ingame-status" = mkHexColor base0A;
-        "--ingameidle-status" = mkHexColor base0C;
-        "--main-background" = mkHexColor base00;
-        "--offline-status" = mkHexColor base03;
-        "--online-status" = mkHexColor cyan;
-        "--purple-color" = mkHexColor base0E;
+    themes = {
+      NEVKO-UI = {
+        enable = true;
+        settings = {
+          removeWindowButtons = "All";
+        };
+        colorOverrides = with colors; {
+          "--custom-accent" = mkHexColor base0B;
+          "--green-color" = mkHexColor green;
+          "--idle-status" = mkHexColor base0D;
+          "--ingame-status" = mkHexColor base0A;
+          "--ingameidle-status" = mkHexColor base0C;
+          "--main-background" = mkHexColor base00;
+          "--offline-status" = mkHexColor base03;
+          "--online-status" = mkHexColor cyan;
+          "--purple-color" = mkHexColor base0E;
+        };
       };
     };
   };
