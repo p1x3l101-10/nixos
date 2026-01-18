450261f041edeb7d68232687a685186873fce882
Pixel <scott.blatt.0b10@gmail.com>
2026-01-18 17:29:16 -0600

    colors

--- modules/home/desktop/shell/noctalia.nix
+++ modules/home/desktop/shell/noctalia.nix
@@ -9,8 +9,22 @@ in {
     settings = (import ./noctalia-settings.nix params) // {
       colors = with config.lib.stylix.colors.withHashtag; {
         mPrimary = mkForce base0B;
-        mSecondary = mkForce base0C;
+        mSecondary = mkForce base0B;
         mTertiary = mkForce base0D;
+        # Stylix colors
+        mOnPrimary = base00;
+        mOnSecondary = base00;
+        mOnTertiary = base00;
+        mError = base08;
+        mOnError = base00;
+        mSurface = base00;
+        mOnSurface = base05;
+        mHover = base04;
+        mOnHover = base00;
+        mSurfaceVariant = base01;
+        mOnSurfaceVariant = base04;
+        mOutline = base02;
+        mShadow = base00;
       };
     };
   };
