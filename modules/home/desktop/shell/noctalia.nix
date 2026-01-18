4b61998b0cfa881aa405747f4be2ee894633810c
Pixel <scott.blatt.0b10@gmail.com>
2026-01-18 16:24:45 -0600

    config

--- modules/home/desktop/shell/noctalia.nix
+++ modules/home/desktop/shell/noctalia.nix
@@ -1,58 +1,9 @@
-{ config, ... }:
+{ ... }@params:
 
 {
   programs.noctalia-shell = {
     enable = true;
     systemd.enable = true;
-    settings = {
-      bar = {
-        density = "compact";
-        position = "top";
-        floating = true;
-        showCapsule = false;
-        widgets = {
-          left = [
-            {
-              id = "ControlCenter";
-              useDistroLogo = true;
-            }
-            {
-              id = "Network";
-            }
-            {
-              id = "Bluetooth";
-            }
-          ];
-          center = [
-            {
-              hideUnoccupied = false;
-              id = "Workspace";
-              labelMode = "none";
-            }
-          ];
-          right = [
-            {
-              alwaysShowPercentage = false;
-              id = "Battery";
-              warningThreshold = 30;
-            }
-            {
-              formatHorizontal = "HH:mm";
-              formatVertical = "HH mm";
-              id = "Clock";
-              useMonospacedFont = true;
-              usePrimaryColor = true;
-            }
-          ];
-        };
-      };
-      general = {
-        avatarImage = "${config.home.homeDirectory}/.face";
-        radiusRatio = 0.2;
-      };
-      location = {
-        name = "Oklahoma City OK";
-      };
-    };
+    settings = import ./noctalia-settings.nix params;
   };
 }
