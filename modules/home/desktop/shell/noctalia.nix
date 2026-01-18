f36830d9d0a2b07f2403ae1d66b2d0abc07e8042
Pixel <scott.blatt.0b10@gmail.com>
2026-01-18 17:35:39 -0600

    fine, ill do it myself...

--- modules/home/desktop/shell/noctalia.nix
+++ modules/home/desktop/shell/noctalia.nix
@@ -2,12 +2,14 @@
 
 let
   inherit (lib) mkForce;
+  inherit (config.lib.stylix) colors;
+  inherit (config.stylix) opacity fonts image;
 in {
   programs.noctalia-shell = {
     enable = true;
     systemd.enable = true;
     settings = (import ./noctalia-settings.nix params) // {
-      colors = with config.lib.stylix.colors.withHashtag; {
+      colors = with colors.withHashtag; {
         mPrimary = mkForce base0B;
         mSecondary = mkForce base0B;
         mTertiary = mkForce base0D;
@@ -26,6 +28,18 @@ in {
         mOutline = base02;
         mShadow = base00;
       };
+      bar.backgroundOpacity = opacity.desktop;
+      bar.capsuleOpacity = opacity.desktop;
+      ui.panelBackgroundOpacity = opacity.desktop;
+      dock.backgroundOpacity = opacity.desktop;
+      osd.backgroundOpacity = opacity.popups;
+      notifications.backgroundOpacity = opacity.popups;
+      ui.fontDefault = fonts.sansSerif.name;
+      ui.fontFixed = fonts.monospace.name;
     };
   };
+  home.file.".cache/noctalia/wallpapers.json" = {
+    text = builtins.toJSON { defaultWallpaper = image; };
+  };
+  stylix.targets.noctalia-shell.enable = false;
 }
