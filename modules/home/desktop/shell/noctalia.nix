c2a3ce4f069f71c5b9278ce8ddfd2680e5a10064
Pixel <scott.blatt.0b10@gmail.com>
2026-01-18 15:54:31 -0600

    fix cfg error

--- modules/home/desktop/shell/noctalia.nix
+++ modules/home/desktop/shell/noctalia.nix
@@ -10,39 +10,41 @@
         position = "top";
         floating = true;
         showCapsule = false;
-        left = [
-          {
-            id = "ControlCenter";
-            useDistroLogo = true;
-          }
-          {
-            id = "Network";
-          }
-          {
-            id = "Bluetooth";
-          }
-        ];
-        center = [
-          {
-            hideUnoccupied = false;
-            id = "Workspace";
-            labelMode = "none";
-          }
-        ];
-        right = [
-          {
-            alwaysShowPercentage = false;
-            id = "Battery";
-            warningThreshold = 30;
-          }
-          {
-            formatHorizontal = "HH:mm";
-            formatVertical = "HH mm";
-            id = "Clock";
-            useMonospacedFont = true;
-            usePrimaryColor = true;
-          }
-        ];
+        widgets = {
+          left = [
+            {
+              id = "ControlCenter";
+              useDistroLogo = true;
+            }
+            {
+              id = "Network";
+            }
+            {
+              id = "Bluetooth";
+            }
+          ];
+          center = [
+            {
+              hideUnoccupied = false;
+              id = "Workspace";
+              labelMode = "none";
+            }
+          ];
+          right = [
+            {
+              alwaysShowPercentage = false;
+              id = "Battery";
+              warningThreshold = 30;
+            }
+            {
+              formatHorizontal = "HH:mm";
+              formatVertical = "HH mm";
+              id = "Clock";
+              useMonospacedFont = true;
+              usePrimaryColor = true;
+            }
+          ];
+        };
       };
       general = {
         avatarImage = "${config.home.homeDirectory}/.face";
