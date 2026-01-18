33556353b97beb39db0cb585290988a05329ed71
Pixel <scott.blatt.0b10@gmail.com>
2026-01-18 17:16:24 -0600

    make colorful

--- modules/home/desktop/shell/noctalia.nix
+++ modules/home/desktop/shell/noctalia.nix
@@ -1,9 +1,17 @@
-{ ... }@params:
+{ config, lib, ... }@params:
 
-{
+let
+  inherit (lib) mkForce;
+in {
   programs.noctalia-shell = {
     enable = true;
     systemd.enable = true;
-    settings = import ./noctalia-settings.nix params;
+    settings = (import ./noctalia-settings.nix params) // {
+      colors = with config.lib.stylix.colors.withHashtag; {
+        mPrimary = mkForce base0B;
+        mSecondary = mkForce base0C;
+        mTertiary = mkForce base0D;
+      };
+    };
   };
 }
