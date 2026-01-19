ccc8235a4b54867afa0b4f561bb0b6f489f6c50c
Pixel <scott.blatt.0b10@gmail.com>
2026-01-18 19:33:13 -0600

    remove nix-your-shell

--- modules/home/desktop/apps/programs/nix-your-shell.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  programs.nix-your-shell = {
-    enable = true;
-    nix-output-monitor.enable = true;
-  };
-}
--- modules/home/desktop/apps/programs/nushell.nix
+++ modules/home/desktop/apps/programs/nushell.nix
@@ -8,7 +8,6 @@
   services.ssh-agent.enableNushellIntegration = true;
   services.gpg-agent.enableNushellIntegration = true;
   programs.starship.enableNushellIntegration = true;
-  programs.nix-your-shell.enableNushellIntegration = true;
   programs.zoxide.enableNushellIntegration = true;
   programs.nix-index.enableNushellIntegration = true;
   programs.lazygit.enableNushellIntegration = true;
