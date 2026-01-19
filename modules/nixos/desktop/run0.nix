e8fd31153d9c64104148a21a1726c68cd26629a8
Pixel <scott.blatt.0b10@gmail.com>
2026-01-19 15:31:54 -0600

    make my own alias

--- modules/nixos/desktop/run0.nix
+++ modules/nixos/desktop/run0.nix
@@ -1,8 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   security.run0 = {
-    enableSudoAlias = true;
+    enableSudoAlias = false; # Done by myself
   };
   security.sudo.enable = false;
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "sudo";
+      text = ''
+        if [[ "$1" == -* ]]; then
+          echo "This script is a sudo-alias to systemd's run0 and does not support any sudo parameters."
+          exit 1
+        fi
+        exec run0 "$@"
+      '';
+    })
+  ];
 }
