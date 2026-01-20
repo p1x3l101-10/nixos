b73e63c9b1e740b4450066ce9d07b7c86fac8759
Pixel <scott.blatt.0b10@gmail.com>
2026-01-19 23:27:34 -0600

    starship prompt

--- modules/home/desktop/apps/programs/starship.nix
+++ modules/home/desktop/apps/programs/starship.nix
@@ -3,5 +3,20 @@
 {
   programs.starship = {
     enable = true;
+    settings = {
+      add_newline = true;
+      character = let
+        symbol = "❯";
+        vimcmd_symbol = "❮";
+      in {
+        format = "$symbol ";
+        success_symbol = "[${symbol}](bold green)";
+        error_symbol = "[${symbol}](bold red)";
+        vimcmd_symbol = "[${vimcmd_symbol}](bold green)";
+        vimcmd_replace_one_symbol = "[${vimcmd_symbol}](bold purple)";
+        vimcmd_replace_symbol = "[${vimcmd_symbol}](bold purple";
+        vimcmd_visual_symbol = "[${vimcmd_symbol}](bold yellow)";
+      };
+    };
   };
 }
