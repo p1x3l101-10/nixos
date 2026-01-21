5b7bba015a198dca5fc49b2f61f2a8684e468d9e
Pixel <scott.blatt.0b10@gmail.com>
2026-01-20 19:11:13 -0600

    add settings

--- modules/home/desktop/apps/programs/keepass.nix
+++ modules/home/desktop/apps/programs/keepass.nix
@@ -4,6 +4,10 @@
   programs.keepassxc = {
     enable = true;
     autostart = true;
+    settings = {
+      Browser.Enabled = true;
+      SSHAgent.Enabled = true;
+    };
   };
   home.packages = with pkgs; [
     keepass-diff
