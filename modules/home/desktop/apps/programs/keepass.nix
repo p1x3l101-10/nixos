578512714d72872538efe1453f09772171d911dc
Pixel <scott.blatt.0b10@gmail.com>
2026-01-20 19:09:48 -0600

    add more keepass

--- modules/home/desktop/apps/programs/keepass.nix
+++ modules/home/desktop/apps/programs/keepass.nix
@@ -1,8 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.keepassxc = {
     enable = true;
     autostart = true;
   };
+  home.packages = with pkgs; [
+    keepass-diff
+    keepass-keeagent
+    keepass-qrcodeview
+    keepass-otpkeyprov
+    kpcli
+  ];
 }
