bc0e19ac3eafc6851208058d7bfdcba4ff359a53
Pixel <scott.blatt.0b10@gmail.com>
2026-01-25 23:16:20 -0600

    downgrade kernel to stable version

--- modules/nixos/desktop/kernel.nix
+++ modules/nixos/desktop/kernel.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }:
+{ ext, ... }:
 
-{
+let
+  # Build failure on unstable right now
+  inherit (ext.stable) pkgs;
+in {
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     kernelParams = [ "usbcore.autosuspend=-1" ];
