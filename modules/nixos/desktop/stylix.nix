f35ff7e73de3e689174ee35a12793c1dbf06c0c9
Pixel <scott.blatt.0b10@gmail.com>
2026-02-01 23:54:03 -0600

    wallpaper

--- modules/nixos/desktop/stylix.nix
+++ modules/nixos/desktop/stylix.nix
@@ -1,27 +1,12 @@
 { pkgs, config, ext, ... }:
 
-{
+let
+  genWallpaper = name: import (./. + "/support/stylix/wallpapers/${name}.nix") { inherit pkgs config ext; };
+in {
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/helios.yaml";
-    #base16Scheme = ./support/stylix/base16/dark-rose.yaml;
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/grayscale-dark.yaml"; # Ooo spooky
-    image = with config.lib.stylix.colors.withHashtag;
-      pkgs.runCommand "cat.png" { } ''
-                pastel=${pkgs.pastel}/bin/pastel
-                SHADOWS=$($pastel darken 0.1 '${base05}' | $pastel format hex)
-                TAIL=$($pastel lighten 0.1 '${base02}' | $pastel format hex)
-                HIGHLIGHTS=$($pastel lighten 0.1 '${base05}' | $pastel format hex)
-
-                ${pkgs.imagemagick}/bin/magick ${ext.assets.img."basecat.png"} \
-        	        -fill '${base00}' -opaque black \
-        	        -fill '${base05}' -opaque white \
-        	        -fill '${base08}' -opaque blue \
-        	        -fill $SHADOWS -opaque gray \
-        	        -fill '${base02}' -opaque orange \
-        	        -fill $TAIL -opaque green \
-        	        -fill $HIGHLIGHTS -opaque brown \
-        	        $out'';
+    image = genWallpaper "cat";
     polarity = "dark";
     targets = { };
     icons = {
---
+++ modules/nixos/desktop/support/stylix/wallpapers/cat.nix
@@ -0,0 +1,20 @@
+{ pkgs, config, ext, ... }:
+
+with config.lib.stylix.colors.withHashtag;
+
+pkgs.runCommand "cat.png" { } ''
+  pastel=${pkgs.pastel}/bin/pastel
+  SHADOWS=$($pastel darken 0.1 '${base05}' | $pastel format hex)
+  TAIL=$($pastel lighten 0.1 '${base02}' | $pastel format hex)
+  HIGHLIGHTS=$($pastel lighten 0.1 '${base05}' | $pastel format hex)
+
+  ${pkgs.imagemagick}/bin/magick ${ext.assets.img."basecat.png"} \
+    -fill '${base00}' -opaque black \
+    -fill '${base05}' -opaque white \
+    -fill '${base08}' -opaque blue \
+    -fill $SHADOWS -opaque gray \
+    -fill '${base02}' -opaque orange \
+    -fill $TAIL -opaque green \
+    -fill $HIGHLIGHTS -opaque brown \
+    $out
+''
