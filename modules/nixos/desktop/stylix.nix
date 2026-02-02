8f2344fe224f7012f6982fd40cf43ad746a46f59
Pixel <scott.blatt.0b10@gmail.com>
2026-02-02 00:08:08 -0600

    make a nixos wallpaper

--- modules/nixos/desktop/stylix.nix
+++ modules/nixos/desktop/stylix.nix
@@ -1,12 +1,21 @@
-{ pkgs, config, ext, ... }:
+{ pkgs, config, ext, lib, ... }:
 
 let
   genWallpaper = name: import (./. + "/support/stylix/wallpapers/${name}.nix") { inherit pkgs config ext; };
+  aspectRatio = (lib.internal.lists.switch [
+    { case = (config.networking.hostName == "pixels-pc"); out = [ 1920 1080 ]; }
+  ] [ null null ]
+  );
 in {
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/helios.yaml";
-    image = genWallpaper "cat";
+    #image = genWallpaper "cat";
+    image = genWallpaper "nixos" {
+      width = builtins.elemAt aspectRatio 0;
+      height = builtins.elemAt aspectRatio 1;
+      logoScale = 4.0;
+    };
     polarity = "dark";
     targets = { };
     icons = {
