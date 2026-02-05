90f737e841dc3946b5006998146aa81968571c16
Pixel <scott.blatt.0b10@gmail.com>
2026-02-04 23:53:28 -0600

    swap back to a lockscreen that supports dbus

--- modules/home/desktop/hyprland/support/hypr-globals.nix
+++ modules/home/desktop/hyprland/support/hypr-globals.nix
@@ -14,8 +14,8 @@ lib.fix (self: {
     roblox = "org.vinegarhq.Sober";
     archiveManager = "xarchiver";
   };
-  #lockCmd = "loginctl lock-session";
-  lockCmd = "noctalia-shell ipc call lockScreen lock";
+  lockCmd = "loginctl lock-session";
+  #lockCmd = "noctalia-shell ipc call lockScreen lock";
   spotlight = "noctalia-shell ipc call launcher toggle";
   appLauncher = self.spotlight;
   clipboardMenu = "noctalia-shell ipc call launcher clipboard";
