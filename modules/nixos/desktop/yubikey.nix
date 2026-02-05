61bfc011c4eef52d8e68a4ae4153284e8a0c2829
Pixel <scott.blatt.0b10@gmail.com>
2026-02-05 00:33:16 -0600

    unable to right now

--- modules/nixos/desktop/yubikey.nix
+++ modules/nixos/desktop/yubikey.nix
@@ -19,10 +19,12 @@
     enable = true;
   };
   hardware.gpgSmartcards.enable = true;
+  /*
   services.udev.extraRules = ''
     ACTION=="remove",\
       ENV{HID_ID}=="0003:00001050:00000407",\
       ENV{HID_NAME}=="Yubico YubiKey OTP+FIDO+CCID",\
       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
   '';
+  */
 }
