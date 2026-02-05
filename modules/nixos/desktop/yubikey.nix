bded53375d962e357f349c46526fcf1141851361
Pixel <scott.blatt.0b10@gmail.com>
2026-02-04 22:59:55 -0600

    polkit

--- modules/nixos/desktop/yubikey.nix
+++ modules/nixos/desktop/yubikey.nix
@@ -15,6 +15,10 @@
       cue = true;
     };
   };
+  security.pam.services = {
+    # Fixes for things that default to password
+    polkit-1.allowNullPassword = true;
+  };
   services.pcscd = {
     enable = true;
   };
