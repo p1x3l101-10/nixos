64eb2936b9a911fe4c97415ff66fe5fd12fe40f4
Pixel <scott.blatt.0b10@gmail.com>
2026-02-05 19:45:21 -0600

    disable patch

--- modules/home/desktop/apps/programs/r2modman.nix
+++ modules/home/desktop/apps/programs/r2modman.nix
@@ -2,10 +2,13 @@
 
 {
   home.packages = [
+    pkgs.r2modman
+    /*
     (pkgs.r2modman.overrideAttrs (oldAttrs: {
       patches = oldAttrs.patches ++ [
         ./support/r2mmp-no-updates.patch
       ];
     }))
+    */
   ];
 }
