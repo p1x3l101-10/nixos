6aa58e311213de62510108da34b591336eb3562a
Pixel <scott.blatt.0b10@gmail.com>
2026-02-07 15:01:22 -0600

    add option searching

--- modules/nixos/base/tools.nix
+++ modules/nixos/base/tools.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.nixos-cli = {
     enable = true;
     useActivationInterface = true;
   };
+  environment.systemPackages = with pkgs; [
+    optnix
+  ];
 }
