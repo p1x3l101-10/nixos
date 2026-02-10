0db58600d709bae621372fc8cf21baa50c28f37a
Pixel <scott.blatt.0b10@gmail.com>
2026-02-10 10:35:03 -0600

    persistance

--- modules/nixos/server/matrix/default.nix
+++ modules/nixos/server/matrix/default.nix
@@ -11,6 +11,5 @@
   ];
   environment.persistence."${globals.dirs.state}/Servers/Matrix".directories = [
     { directory = "/var/lib/matrix-synapse"; user = "matrix-synapse"; group = "matrix-synapse"; }
-    { directory = "/var/lib/matrix-appservice-discord"; user = "matrix-synapse"; group = "matrix-synapse"; }
   ];
 }
