446ffc46ed0bfe29d5bc89330cd66b3eb2baa5da
Pixel <scott.blatt.0b10@gmail.com>
2026-02-11 15:14:41 -0600

    change rule type

--- modules/nixos/server/ensurePerms/default.nix
+++ modules/nixos/server/ensurePerms/default.nix
@@ -6,35 +6,35 @@ let
 in {
   systemd.tmpfiles.settings = {
     "10-nextcloud-permissions" = mkIf config.services.nextcloud.enable {
-      "${state}/Servers/Nextcloud/var/lib/nextcloud/*".e = {
+      "${state}/Servers/Nextcloud/var/lib/nextcloud".Z = {
         user = "nextcloud";
         group = "nextcloud";
       };
-      "${keys}/nextcloud/*".e = {
+      "${keys}/nextcloud/*".Z = {
         user = "nextcloud";
         group = "nextcloud";
       };
     };
     "10-matrix-permissions" = mkIf config.services.matrix-synapse.enable {
-      "${state}/Servers/Matrix/var/lib/matrix-synapse/*".e = {
+      "${state}/Servers/Matrix/var/lib/matrix-synapse".Z = {
         user = "matrix-synapse";
         group = "matrix-synapse";
       };
-      "${keys}/Matrix/keys.yaml".e = {
+      "${keys}/Matrix/keys.yaml".Z = {
         user = "matrix-synapse";
         group = "matrix-synapse";
       };
     };
     "10-email-permissions" = mkIf config.services.maddy.enable {
-      "${state}/Servers/EMail/var/lib/maddy/*".e = {
+      "${state}/Servers/EMail/var/lib/maddy".Z = {
         user = "maddy";
         group = "maddy";
       };
-      "${state}/Servers/EMail/var/lib/rspamd/*".e = {
+      "${state}/Servers/EMail/var/lib/rspamd".Z = {
         user = "rspamd";
         group = "rspamd";
       };
-      "${keys}/email/*".e = {
+      "${keys}/email/*".Z = {
         user = "maddy";
         group = "maddy";
       };
