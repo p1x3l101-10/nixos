f5acd6ece05b31fd2428aa42fbb9a9c5a39e2c5e
Pixel <scott.blatt.0b10@gmail.com>
2026-02-11 12:59:53 -0600

    fix nginx

--- modules/nixos/server/email/mta-sts.nix
+++ modules/nixos/server/email/mta-sts.nix
@@ -2,7 +2,7 @@
 
 let
 in {
-  services.nginx.virtualHosts."${config.networking.domain}".locations."/.well-known/mta-sts.txt".alias = pkgs.writeText "mta-sts.txt" ''
+  services.nginx.virtualHosts."${config.networking.domain}".locations."= /.well-known/mta-sts.txt".root = pkgs.writeText "mta-sts.txt" ''
     version: STSv1
     mode: enforce
     max_age: 604800
