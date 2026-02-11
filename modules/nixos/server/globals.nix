d41e75988c928df4c77ba8d37c21bebcb71218c7
Pixel <scott.blatt.0b10@gmail.com>
2026-02-10 21:37:11 -0600

    change basename

--- modules/nixos/server/globals.nix
+++ modules/nixos/server/globals.nix
@@ -5,7 +5,7 @@ lib.fix (self: {
   server = {
     dns = {
       # DNS data and helpers
-      basename = "piplup.pp.ua"; # Basename for dns
+      basename = "exsmachina.org"; # Basename for dns
       exists = (if self.server.dns.basename != "" then true else false); # True when dns is not empty, otherwise false
       required = attrs: (lib.mkIf self.server.dns.exists attrs); # mkIf wrapper to check if the dns is set or not
     };
