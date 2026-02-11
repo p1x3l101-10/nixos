b3d5940b1880f7f217bc4c316e0033614a499bf6
Pixel <scott.blatt.0b10@gmail.com>
2026-02-11 14:02:42 -0600

    tls

--- modules/nixos/server/email/tls.nix
+++ modules/nixos/server/email/tls.nix
@@ -4,20 +4,20 @@ let
   inherit (globals.dirs) keys;
 in {
   services.maddy = {
+    tls = {
+      loader = "acme";
+      extraConfig = ''
+        email scott.blatt.0b10@gmail.com
+        agreed
+        challenge dns-01
+        dns cloudflare {
+          api_token "{env:CLOUDFLARE_DNS_API_KEY}"
+        }
+      '';
+    };
     # Enable TLS listeners. Configuring this via the module is not yet
     # implemented, see https://github.com/NixOS/nixpkgs/pull/153372
     config = ''
-      tls {
-        loader acme {
-          email scott.blatt.0b10@gmail.com
-          agreed
-          host ${config.services.maddy.primaryDomain}
-          challenge dns-01
-          dns cloudflare {
-            api_token "{env:CLOUDFLARE_DNS_API_KEY}"
-          }
-        }
-      }
       imap tls://0.0.0.0:993 tcp://0.0.0.0:143 {
         auth &local_authdb
         storage &local_mailboxes
