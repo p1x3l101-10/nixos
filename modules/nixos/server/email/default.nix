b604244ebc7aca84b612051409ee93e7fbb0c62a
Pixel <scott.blatt.0b10@gmail.com>
2026-02-11 13:38:51 -0600

    forwarding

--- modules/nixos/server/email/default.nix
+++ modules/nixos/server/email/default.nix
@@ -1,4 +1,4 @@
-{ globals, config, ... }:
+{ globals, config, lib, ... }:
 
 let
   host = config.networking.domain;
@@ -20,6 +20,11 @@ in {
   environment.persistence."${state}/Servers/EMail".directories = [
     "/var/lib/maddy"
   ];
+  networking.sshForwarding.ports = (lib.optionals globals.server.dns.exists [
+    { host = 25; remote = 2555; }
+    { host = 143; remote = 1430; }
+    { host = 587; remote = 5870; }
+  ]);
   imports = [
     ./mta-sts.nix
     ./rspamd.nix
