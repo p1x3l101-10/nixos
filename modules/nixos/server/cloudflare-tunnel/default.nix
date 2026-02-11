15c3d026bb48f344b42c6de8d7fbe5e54a627ef6
Pixel <scott.blatt.0b10@gmail.com>
2026-02-11 10:12:35 -0600

    config tunnel

--- modules/nixos/server/cloudflare-tunnel/default.nix
+++ modules/nixos/server/cloudflare-tunnel/default.nix
@@ -1,8 +1,17 @@
-{ globals, ... }:
+{ globals, config, ... }:
 
 {
   services.cloudflared = {
-    enable = true;
+    enable = globals.server.dns.exists;
     certificateFile = "${globals.dirs.keys}/cloudflared/cert.pem";
+    tunnels = {
+      "d164644e-64b8-6d50-dd06-5ef63f334c99" = {
+        certificateFile = "${globals.dirs.keys}/cloudflared/d164644e64b86d50dd065ef63f334c99.pem";
+        default = "http_status:404";
+        ingress = {
+          "*.${globals.server.dns.basename}" = "http://localhost:443"
+        };
+      };
+    };
   };
 }
