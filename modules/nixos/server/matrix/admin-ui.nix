801f7f15b20e25c344f49b0fb3eb849dc30728a2
Pixel <scott.blatt.0b10@gmail.com>
2026-02-11 23:42:51 -0600

    modify domain name

--- modules/nixos/server/matrix/admin-ui.nix
+++ modules/nixos/server/matrix/admin-ui.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 
 {
-  services.nginx.virtualHosts."admin.matrix.${config.networking.domain}" = {
+  services.nginx.virtualHosts."admin-matrix.${config.networking.domain}" = {
     enableACME = true;
     forceSSL = true;
     locations."/".root = pkgs.synapse-admin-etkecc;
