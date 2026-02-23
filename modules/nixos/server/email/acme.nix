b08cfb05ff8a00b5c980f853f842371c87aa05ec
Pixel <scott.blatt.0b10@gmail.com>
2026-02-23 13:51:00 -0600

    alter method

--- modules/nixos/server/email/acme.nix
+++ modules/nixos/server/email/acme.nix
@@ -2,57 +2,27 @@
 
 let
   domain = "mail.${config.networking.domain}";
-  inherit (config.security.acme) certs;
-  ssl = let
-    inherit (certs."${domain}") directory;
-  in {
-    key = "${directory}/key.pem";
-    cert = "${directory}/cert.pem";
-  };
 in {
   services.nginx.virtualHosts."${domain}" = {
     enableACME = true;
     globalRedirect = config.networking.domain;
   };
-  services.opensmtpd.serverConfiguration = ''
-    pki ${domain} cert "/var/lib/smtpd/cert.pem"
-    pki ${domain} key "/var/lib/smtpd/key.pem"
+  services.opensmtpd.serverConfiguration = let
+    creds = "/run/credentials/opensmtpd.service"
+  in ''
+    pki ${domain} cert "${creds}/cert.pem"
+    pki ${domain} key "${creds}/key.pem"
   '';
-  systemd = {
-    tmpfiles.settings."50-smtpd"."/var/lib/smtpd".d = {
-      user = "root";
-      group = "root";
-      mode = "0755";
-    };
-    paths = {
-      copy-smtpd-keys = {
-        # Wait for acme to alter the keys
-        requiredBy = [ "acme-renew-${domain}.timer" "opensmtpd.service" ];
-        before = [ "opensmtpd.service" ];
-        pathConfig.PathModified = [
-          ssl.key
-          ssl.cert
-        ];
-      };
-    };
-    services = {
-      copy-smtpd-keys = {
-        unitConfig.RequiresMountsFor = "${certs."${domain}".directory} /var/lib/smtpd";
-        # Need tempdir
-        requires = [ "systemd-tmpfiles-setup.service" ];
-        after = [ "systemd-tmpfiles-setup.service" ];
-        # Force opensmtpd to restart when the keys change
-        before = [ "opensmtpd.service" ];
-        requiredBy = [ "opensmtpd.service" ];
-        partOf = [ "opensmtpd.service" ];
-        script = ''
-          rm -f /var/lib/smtpd/key.pem /var/lib/smtpd/cert.pem
-          cp ${ssl.key} /var/lib/smtpd/key.pem
-          chown root:root /var/lib/smtpd/key.pem
-          cp ${ssl.cert} /var/lib/smtpd/cert.pem
-          chown root:root /var/lib/smtpd/cert.pem
-        '';
-      };
-    };
+  systemd.services.opensmtpd = {
+    requires = [ "acme-${domain}.service" ];
+    serviceConfig.LoadCredential = let
+      inherit (config.security.acme.certs."${domain}") directory;
+    in [
+      "key.pem:${directory}/key.pem"
+      "cert.pem:${directory}/cert.pem"
+    ];
   };
+  security.acme.certs."${domain}".postRun = ''
+    systemctl restart opensmtpd
+  '';
 }
