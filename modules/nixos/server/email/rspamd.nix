06c27ef04f91b1845dc3ab279bc88c046b005fb1
Pixel <scott.blatt.0b10@gmail.com>
2026-02-23 17:34:36 -0600

    no maddy

--- modules/nixos/server/email/rspamd.nix
+++ modules/nixos/server/email/rspamd.nix
@@ -3,11 +3,6 @@
 {
   services.rspamd = {
     enable = true;
-    locals."dkim_signing.conf".text = ''
-      selector = "default";
-      domain = "project-insanity.org";
-      path = "/var/lib/maddy/dkim_keys/$domain_$selector.key";
-    '';
   };
   systemd.services.rspamd.serviceConfig.SupplementaryGroups = [ "smtpd" ];
   environment.persistence."${globals.dirs.state}/Servers/EMail".directories = [
