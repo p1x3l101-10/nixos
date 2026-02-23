cedfe4a2e68593b7f9014764e7e5ba430793c79d
Pixel <scott.blatt.0b10@gmail.com>
2026-02-23 13:23:12 -0600

    update key mechanism

--- modules/nixos/server/email/acme.nix
+++ modules/nixos/server/email/acme.nix
@@ -15,7 +15,43 @@ in {
     globalRedirect = config.networking.domain;
   };
   services.opensmtpd.serverConfiguration = ''
-    pki ${domain} cert "${ssl.cert}"
-    pki ${domain} key "${ssl.key}"
+    pki ${domain} cert "/var/lib/smtpd/cert.pem"
+    pki ${domain} key "/var/lib/smtpd/key.pem"
   '';
+  systemd = {
+    tmpfiles.settings."50-smtpd"."/var/lib/smtpd".d = {
+      user = "root";
+      group = "root";
+      argument = "0700";
+    };
+    paths = {
+      copySmtpdKeys = {
+        # Wait for acme to alter the keys
+        requiredBy = [ "acme-renew-${domain}.timer" "opensmtpd.service" ];
+        before = [ "opensmtpd.service" ];
+        pathConfig.PathModified = [
+          ssl.key
+          ssl.cert
+        ];
+      };
+    };
+    services = {
+      copySmtpdKeys = {
+        # Need tempdir
+        requires = [ "systemd-tmpfiles-setup.service" ];
+        after = [ "systemd-tmpfiles-setup.service" ];
+        # Force opensmtpd to restart when the keys change
+        before = [ "opensmtpd.service" ];
+        requiredBy = [ "opensmtpd.service" ];
+        partOf = [ "opensmtpd.service" ];
+        script = ''
+          rm -f /var/lib/smtpd/key.pem /var/lib/smtpd/cert.pem
+          cp ${ssl.key} /var/lib/smtpd/key.pem
+          chown root:root /var/lib/smtpd/key.pem
+          cp ${ssl.cert} /var/lib/smtpd/cert.pem
+          chown root:root /var/lib/smtpd/cert.pem
+        '';
+      };
+    };
+  };
 }
