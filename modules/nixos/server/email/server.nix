093a1651b79b7ddc74437ffc824e3c489b6cbe46
Pixel <scott.blatt.0b10@gmail.com>
2026-02-25 15:15:09 -0600

    fix password

--- modules/nixos/server/email/server.nix
+++ modules/nixos/server/email/server.nix
@@ -14,7 +14,7 @@ in {
         hashedPassword = "$y$j9T$xIqsbrS7kn13DWVH06nf11$ewQiB0.UkYcVSCIU5XqfuV9Ej4ss5.m1ATkks7YSU/9";
       };
       "pixel@${domain}" = {
-        hashedPassword = "$y$j9T$ChefBxNe7LttV2jeKpeo00$DZJxTcIlROpaFyfUV109btSMI9khiDxDvugU/dUnLD3";
+        hashedPassword = "$y$j9T$A4sSsk3aGXCkvq3SVAxgD/$1yiG7lNlWfDNfpWUlD0Onezk2jhz1uB.Ds4YCnKQPzC";
       };
     };
   };
