b87c059c6445eb67722fbc9261cb207dd6c2adb5
Pixel <scott.blatt.0b10@gmail.com>
2026-02-24 21:52:08 -0600

    setup peers and remove comments

--- modules/nixos/base/yggdrasil.nix
+++ modules/nixos/base/yggdrasil.nix
@@ -7,39 +7,18 @@
     persistentKeys = true;
     denyDhcpcdInterfaces = [ "tap*" ];
     settings = {
-      # List of outbound peer connection strings (e.g. tls://a.b.c.d:e or
-      # socks://a.b.c.d:e/f.g.h.i:j). Connection strings can contain options,
-      # see https://yggdrasil-network.github.io/configurationref.html#peers.
-      # Yggdrasil has no concept of bootstrap nodes - all network traffic
-      # will transit peer connections. Therefore make sure to only peer with
-      # nearby nodes that have good connectivity and low latency. Avoid adding
-      # peers to this list from distant countries as this will worsen your
-      # node's connectivity and performance considerably.
-      Peers = [];
-
-      # List of connection strings for outbound peer connections in URI format,
-      # arranged by source interface, e.g. { "eth0" = [ "tls://a.b.c.d:e" ] }.
-      # You should only use this option if your machine is multi-homed and you
-      # want to establish outbound peer connections on different interfaces.
-      # Otherwise you should use "Peers".
-      InterfacePeers = {};
-
-      # Listen addresses for incoming connections. You will need to add
-      # listeners in order to accept incoming peerings from non-local nodes.
-      # This is not required if you wish to establish outbound peerings only.
-      # Multicast peer discovery will work regardless of any listeners set
-      # here. Each listener should be specified in URI format as above, e.g.
-      # tls://0.0.0.0:0 or tls://[::]:0 to listen on all interfaces.
+      Peers = [
+        # Public peers
+        # NOTE: these peers are taken from this list "https://publicpeers.neilalexander.dev/"
+        # Dont oversaturate it.
+        "tls://ygg.jjolly.dev:3443"
+        "quic://ygg4.mk16.de:1339?key=000000573433e11f23768b078bcdc10b42712a7b131d6d04b82042ffc0c97df0"
+        "tls://longseason.1200bps.xyz:13122"
+        "tls://ygg.mnpnk.com:443"
+        # Less public peers
+        # NOTE: I can do a total free for all here if I want
+      ];
       Listen = [];
-
-      # Configuration for which interfaces multicast peer discovery should be
-      # enabled on. Regex is a regular expression which is matched against an
-      # interface name, and interfaces use the first configuration that they
-      # match against. Beacon controls whether or not your node advertises its
-      # presence to others, whereas Listen controls whether or not your node
-      # listens out for and tries to connect to other advertising nodes. See
-      # https://yggdrasil-network.github.io/configurationref.html#multicastinterfaces
-      # for more supported options.
       MulticastInterfaces = [
         {
           Regex = "en.*";
@@ -60,34 +39,10 @@
           Password = "";
         }
       ];
-    
-      # List of peer public keys to allow incoming peering connections
-      # from. If left empty/undefined then all connections will be allowed
-      # by default. This does not affect outgoing peerings, nor does it
-      # affect link-local peers discovered via multicast.
-      # WARNING = THIS IS NOT A FIREWALL and DOES NOT limit who can reach
-      # open ports or services running on your machine!
       AllowedPublicKeys = [];
-    
-      # Local network interface name for TUN adapter, or "auto" to select
-      # an interface automatically, or "none" to run without TUN.
       IfName = "auto";
-    
-      # Maximum Transmission Unit (MTU) size for your local TUN interface.
-      # Default is the largest supported size for your platform. The lowest
-      # possible value is 1280.
       IfMTU = 65535;
-    
-      # By default, nodeinfo contains some defaults including the platform,
-      # architecture and Yggdrasil version. These can help when surveying
-      # the network and diagnosing network routing problems. Enabling
-      # nodeinfo privacy prevents this, so that only items specified in
-      # "NodeInfo" are sent back if specified.
       NodeInfoPrivacy = false;
-    
-      # Optional nodeinfo. This must be a { "key" = "value", ... } map
-      # or set as null. This is entirely optional but, if set, is visible
-      # to the whole network on request.
       NodeInfo = {};
     };
   };
