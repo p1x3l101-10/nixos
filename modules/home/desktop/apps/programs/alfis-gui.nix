ec061e15718c662dba38f6f205b00d3162e11585
Pixel <scott.blatt.0b10@gmail.com>
2026-02-24 21:39:05 -0600

    minor typo

--- modules/home/desktop/apps/programs/alfis-gui.nix
+++ modules/home/desktop/apps/programs/alfis-gui.nix
@@ -6,7 +6,7 @@
     text = ''
       # Needs to run as root
       if [[ $EUID -ne 0 ]]; then
-        exec sudo /bin/env WAYLAND_DISPLAY="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY"  XDG_RUNTIME_DIR=/user/run/0 "$0" "$@"
+        exec sudo /usr/bin/env WAYLAND_DISPLAY="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY"  XDG_RUNTIME_DIR=/user/run/0 "$0" "$@"
       fi
 
       cd /var/lib/alfis
