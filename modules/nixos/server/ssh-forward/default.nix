afaff7603d8824a730588726ed848af5aba0ba93
Pixel <scott.blatt.0b10@gmail.com>
2026-02-26 13:35:35 -0600

    fix key

--- modules/nixos/server/ssh-forward/default.nix
+++ modules/nixos/server/ssh-forward/default.nix
@@ -8,7 +8,7 @@
   networking.sshForwarding = {
     enable = true;
     trustedHostKeys = [
-      "[srv01.exsmachina.org]:2222 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDu8VS/AEY5smqFoxM+hRKoCp5cTofBcNKhgyni1qk4A5XKTPpn5YTpbGRMPx5Xsl4xp3Q5y/gs3Y+j+qvzE+A0="
+      "[srv01.exsmachina.org]:2222 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+JEJ7SczOJCcn/bPGUySPXH0FUsXl8C2/wFY1r3g1h"
     ];
     sshPort = 2222;
     keyFile = "/nix/host/keys/ssh-tunnel/id.key";
