eade8d43e3aef143ddb7868f41f19c335e7a41e2
Pixel <scott.blatt.0b10@gmail.com>
2026-02-26 13:24:05 -0600

    fix hardware

--- systems/hetzner-vps/hardware-configuration.nix
+++ systems/hetzner-vps/hardware-configuration.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -12,4 +12,16 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  systemd.network.networks."10-wan" = {
+    networkConfig.DHCP = "no";
+    address = [
+      "5.78.134.177/32"
+      "2a01:4ff:1f0:c947::/64"
+    ];
+    routes = [
+      { routeConfig.Gateway = "fe80::1"; }
+      { routeConfig = { Destination = "172.31.1.1"; }; }
+      { routeConfig = { Gateway = "172.31.1.1"; GatewayOnLink = true; }; }
+    ];
+  };
 }
