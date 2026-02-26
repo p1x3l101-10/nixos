1d382b1cad5e8e7995f2a05dc8eb61af876f45db
Pixel <scott.blatt.0b10@gmail.com>
2026-02-26 11:56:46 -0600

    fixes

--- systems/hetzner-vps/default.nix
+++ systems/hetzner-vps/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   imports = [ ./hardware-configuration.nix ./kvm.nix ./disko-config.nix ];
   networking.hostName = "hetzner-vps";
@@ -6,7 +6,7 @@
   environment.etc.machine-id.text = "6b4d18e892d8456fafb8e4dc441fad92";
 
   system.useSecureBoot = false;
-  boot.loader.systemd-boot.enable = false;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.grub = {
     enable = true;
