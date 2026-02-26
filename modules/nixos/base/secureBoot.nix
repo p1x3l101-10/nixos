71b1995e39b67e12e45023f91fe964444cf3db9c
Pixel <scott.blatt.0b10@gmail.com>
2026-02-26 11:49:23 -0600

    disable lanzeboote

--- modules/nixos/base/secureBoot.nix
+++ modules/nixos/base/secureBoot.nix
@@ -4,41 +4,48 @@ let
   bootDir = config.boot.loader.efi.efiSysMountPoint;
 in
 {
-  boot = {
-    loader.systemd-boot.enable = lib.mkForce false;
-    loader.grub.enable = lib.mkForce false;
-    lanzaboote = {
-      enable = true;
-      pkiBundle = "${keydir}";
-    };
+  options.system.useSecureBoot = lib.mkOption {
+    description = "Forces system to use lanzeboote for booting ukis";
+    type = lib.types.bool;
+    default = true;
   };
-  # Make sure keys are present for system
-  systemd.tmpfiles.settings."01-secureboot" = {
-    "/var/lib/sbctl".L = {
-      user = "root";
-      group = "root";
-      mode = "0700";
-      argument = keydir;
+  config = lib.mkIf config.system.useSecureBoot {
+    boot = {
+      loader.systemd-boot.enable = lib.mkForce false;
+      loader.grub.enable = lib.mkForce false;
+      lanzaboote = {
+        enable = true;
+        pkiBundle = "${keydir}";
+      };
     };
-  };
-  # Make sure keys are present for bootloader
-  # Only copy pubKeys
-  system.activationScripts = {
-    secureBoot-keys.text = ''
-      ## Copy UEFI Secure Boot public keys to ${bootDir}
-      # If the keys already exist, skip this step
-      if [ ! -e "${bootDir}/loader/keys/local" ]; then 
-        # Announce our precence
-        echo "Installing secureBoot keys..."
-
-        # Ensure needed directories exist
-        mkdir -p ${bootDir}/loader/keys/local
-
-        # Copy variables
-        cp ${keydir}/PK/PK.auth ${bootDir}/loader/keys/local
-        cp ${keydir}/KEK/KEK.auth ${bootDir}/loader/keys/local
-        cp ${keydir}/db/db.auth ${bootDir}/loader/keys/local
-      fi
-    '';
+    # Make sure keys are present for system
+    systemd.tmpfiles.settings."01-secureboot" = {
+      "/var/lib/sbctl".L = {
+        user = "root";
+        group = "root";
+        mode = "0700";
+        argument = keydir;
+      };
+    };
+    # Make sure keys are present for bootloader
+    # Only copy pubKeys
+    system.activationScripts = {
+      secureBoot-keys.text = ''
+        ## Copy UEFI Secure Boot public keys to ${bootDir}
+        # If the keys already exist, skip this step
+        if [ ! -e "${bootDir}/loader/keys/local" ]; then 
+          # Announce our precence
+          echo "Installing secureBoot keys..."
+  
+          # Ensure needed directories exist
+          mkdir -p ${bootDir}/loader/keys/local
+  
+          # Copy variables
+          cp ${keydir}/PK/PK.auth ${bootDir}/loader/keys/local
+          cp ${keydir}/KEK/KEK.auth ${bootDir}/loader/keys/local
+          cp ${keydir}/db/db.auth ${bootDir}/loader/keys/local
+        fi
+      '';
+    };
   };
 }
--- systems/hetzner-vps/default.nix
+++ systems/hetzner-vps/default.nix
@@ -4,11 +4,16 @@
   networking.hostName = "hetzner-vps";
   networking.hostId = "6b4d18e8";
   environment.etc.machine-id.text = "6b4d18e892d8456fafb8e4dc441fad92";
+
+  system.useSecureBoot = false;
   boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.lanzaboote.enable = false;
   boot.loader.grub = {
     enable = true;
     efiSupport = false;
     copyKernels = false;
     storePath = "/store";
+    device = "/dev/sda";
   };
 }
