1a0056212c7fd9472db3956f9ed2f8608c3accb3
Pixel <scott.blatt.0b10@gmail.com>
2026-02-25 22:32:46 -0600

    small fixes

--- modules/nixos/server/email/support/smtp.pl
+++ modules/nixos/server/email/support/smtp.pl
@@ -2,8 +2,8 @@
 use warnings;
 use strict;
 my @cmd = ($0.'.orig', @ARGV);
-my $closed = system 'lsof -i :23649 >/dev/null 2>/dev/null';
+my $closed = system 'lsof -i :1081 >/dev/null 2>/dev/null';
 my @preload
-push @preload; 'libproxychains.so' if !$closed;
+push @preload; 'libproxychains4.so' if !$closed;
 $ENV{LD_PRELOAD} = join ':', @preload if @preload;
 exec { $cmd[0] } 'smtp', @cmd[1..$#cmd];
