967b3e137246b99eb1d76124bb437f09cbd0cf46
Pixel <scott.blatt.0b10@gmail.com>
2026-02-27 09:19:51 -0600

    ip fixes

--- systems/hetzner-vps/wireguard.nix
+++ systems/hetzner-vps/wireguard.nix
@@ -3,6 +3,6 @@
 {
   systemd.network.networks."50-wg0".address = [
     "10.64.186.61/32"
-    "fd2a:bf8f:acb9::/64"
+    "fd31:8b54:ccba::acb9::/64"
   ];
 }
--- systems/pixels-server/wireguard.nix
+++ systems/pixels-server/wireguard.nix
@@ -3,6 +3,6 @@
 {
   systemd.network.networks."50-wg0".address = [
     "10.64.186.60/32"
-    "fd31:8b54:ccba::/64"
+    "fd31:8b54:ccba::ccba/64"
   ];
 }
