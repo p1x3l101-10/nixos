edcc2eed968706dd25ca5a419af081bd7c01eac8
Pixel <scott.blatt.0b10@gmail.com>
2026-02-27 09:46:37 -0600

    fix issue

--- modules/nixos/vps/server-base/ip-block.nix
+++ modules/nixos/vps/server-base/ip-block.nix
@@ -11,7 +11,6 @@
     bantime = "24h"; # Ban for one day on the first fail
     bantime-increment = {
       enable = true;
-      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
       multipliers = "1 2 4 8 16 32 64";
       maxtime = "168h"; # Do not ban for more than 1 week
       overalljails = true; # Calculate the bantime based on all the violations
