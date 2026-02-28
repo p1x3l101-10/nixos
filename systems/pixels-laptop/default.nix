5e58ccf958b1ba1aa6e32d2b2210f5447c8b4c28
Pixel <scott.blatt.0b10@gmail.com>
2026-02-27 18:32:41 -0600

    wireless networking

--- systems/pixels-laptop/default.nix
+++ systems/pixels-laptop/default.nix
@@ -1,6 +1,12 @@
 { config, ... }:
 {
-  imports = [ ./hardware-configuration.nix ./kvm.nix ./disko-config.nix ];
+  imports = [
+    ./hardware-configuration.nix
+    ./kvm.nix
+    ./disko-config.nix
+    ./network.nix
+    ./power-saving.nix
+  ];
   networking.hostName = "pixels-laptop";
   networking.hostId = "dd1c170a";
   environment.etc.machine-id.text = "dd1c170ad32148e09e7014adf076f233";
---
+++ systems/pixels-laptop/network.nix
@@ -0,0 +1,56 @@
+{ globals, ... }:
+
+{
+  systemd.network.networks = {
+    "10-wired" = {
+      name = "enp2s0";
+      DHCP = "yes";
+      linkConfig.RequiredForOnline = "routable";
+    };
+    "10-wireless" = {
+      name = "wlp0s20f3";
+      DHCP = "yes";
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
+  networking.iwd = {
+    enable = true;
+    settings = {
+      General = {
+        EnableNetworkConfiguration = false; # NetworkD is handling that instead.
+        AddressRandomization = "network";
+        AddressRandomizationRange = "full";
+        SystemdEncrypt = "iwd-secret";
+        Country = "us";
+      };
+    };
+  };
+  systemd.services = let
+    keydir = "${keydir}";
+  in {
+    # Dont start IWD until there is a valid credential
+    iwd = {
+      unitConfig.ConditionPathExists "${keydir}/iwd-secret.cred";
+      serviceConfig.LoadCredentialEncrypted = "iwd-secret:/${keydir}/iwd-secret.cred";
+    };
+    iwd-ensure-credentials = {
+      description = "Generate IWD credentials";
+      serviceConfig.Type = "oneshot";
+      wantedBy = [ "iwd.service" ];
+      before = [ "iwd.service" ];
+      unitConfig.ConditionPathExists = [ "!${keydir}/iwd-secret.cred" ];
+      # Generate a random encryption password for iwd to work with
+      script = ''
+        set -euxo pipefail
+        mkdir -p "${keydir}"
+        cd "${keydir}"
+        umask 377
+        cat /dev/urandom | tr -dc 'A-Za-z0-9!@#$%^&*_-' | head -c 64 | systemd-creds --tpm2-device=auto --name=iwd-secret encrypt - "${keydir}/iwd-secret.cred"
+        chmod 0400 "${keydir}/iwd-secret.cred"
+      '';
+    };
+  };
+  environment.persistence."/nix/host/state/System".directories = [
+    "/var/lib/iwd"
+  ];
+}
