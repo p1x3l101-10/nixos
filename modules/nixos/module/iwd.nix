54c6b08e0568e1a60cb131cc29584224f3134c8a
Pixel <scott.blatt.0b10@gmail.com>
2026-02-27 19:17:11 -0600

    move to module

---
+++ modules/nixos/module/iwd.nix
@@ -0,0 +1,44 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkOption mkIf types;
+  cfg = config.networking.wireless.iwd.encryptDB;
+in {
+  options.networking.wireless.iwd.encryptDB = {
+    enable = mkOption {
+      description = "Uses systemd-creds with the tpm chip to encrypt iwd's passwords";
+      type = types.bool;
+      default = false;
+    };
+    keyLocation = mkOption {
+      description = "Where to store the credential file";
+      type = types.externalPath;
+      default = "/var/lib/iwd/iwd-secret.cred";
+    };
+  };
+  config = mkIf cfg.enable {
+    systemd.services = {
+      # Dont start IWD until there is a valid credential
+      iwd = {
+        unitConfig.ConditionPathExists = "${cfg.keyLocation}";
+        serviceConfig.LoadCredentialEncrypted = "iwd-secret:/${cfg.keyLocation}";
+      };
+      iwd-ensure-credentials = {
+        description = "Generate IWD credentials";
+        serviceConfig.Type = "oneshot";
+        wantedBy = [ "iwd.service" ];
+        before = [ "iwd.service" ];
+        # Generate a random encryption password for iwd to work with
+        script = ''
+          set -euxo pipefail
+          [[ -e "${cfg.keyLocation}" ]] && exit 0
+          mkdir -p "$(dirname "${cfg.keyLocation}")"
+          cd "$(dirname "${cfg.keyLocation}")"
+          umask 377
+          cat /dev/urandom | tr -dc 'A-Za-z0-9!@#$%^&*_-' | head -c 512 | systemd-creds --tpm2-device=auto --name=iwd-secret encrypt - "${cfg.keyLocation}"
+          chmod 0400 "${cfg.keyLocation}"
+        '';
+      };
+    };
+  };
+}
--- systems/pixels-pc/network.nix
+++ systems/pixels-pc/network.nix
@@ -15,6 +15,10 @@
   };
   networking.wireless.iwd = {
     enable = true;
+    encryptDB = {
+      enable = true;
+      keyLocation = "${globals.dirs.keys}/iwd/iwd-secret.cred";
+    };
     settings = {
       General = {
         EnableNetworkConfiguration = false; # NetworkD is handling that instead.
@@ -23,31 +27,6 @@
         SystemdEncrypt = "iwd-secret";
         Country = "us";
       };
-    };
-  };
-  systemd.services = let
-    keydir = "${globals.dirs.keys}/iwd";
-  in {
-    # Dont start IWD until there is a valid credential
-    iwd = {
-      unitConfig.ConditionPathExists = "${keydir}/iwd-secret.cred";
-      serviceConfig.LoadCredentialEncrypted = "iwd-secret:/${keydir}/iwd-secret.cred";
-    };
-    iwd-ensure-credentials = {
-      description = "Generate IWD credentials";
-      serviceConfig.Type = "oneshot";
-      wantedBy = [ "iwd.service" ];
-      before = [ "iwd.service" ];
-      # Generate a random encryption password for iwd to work with
-      script = ''
-        set -euxo pipefail
-        [[ -e "${keydir}/iwd-secret.cred" ]] && exit 0
-        mkdir -p "${keydir}"
-        cd "${keydir}"
-        umask 377
-        cat /dev/urandom | tr -dc 'A-Za-z0-9!@#$%^&*_-' | head -c 64 | systemd-creds --tpm2-device=auto --name=iwd-secret encrypt - "${keydir}/iwd-secret.cred"
-        chmod 0400 "${keydir}/iwd-secret.cred"
-      '';
     };
   };
   environment.persistence."/nix/host/state/System".directories = [
