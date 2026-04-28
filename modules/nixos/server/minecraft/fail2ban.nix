c5d7bcec7c63780cd7a01bede63a1c9d83efef74
Pixel <scott.blatt.0b10@gmail.com>
2026-04-28 13:31:18 -0500

    streamline

--- modules/nixos/server/minecraft/fail2ban.nix
+++ modules/nixos/server/minecraft/fail2ban.nix
@@ -1,36 +1,19 @@
 { lib, ... }:
 
 {
-  services.fail2ban.jails = {
-    minecraft-whitelist = {
-      settings = {
-        filter = "minecraft-whitelist";
-        logpath = "/var/lib/minecraft/data/logs/latest.log";
-        maxretry = "1"; # They will tell me if they are not whitelisted, so only allow one chance to prevent possible exploits
-        bantime = "1h";
-      };
-    };
-    minecraft-connection-spam = {
-      settings = {
-        filter = "minecraft-connection-spam";
-        logpath = "/var/lib/minecraft/data/logs/latest.log";
-        maxretry = "3"; # Allow for people with terrible internet to join
-        bantime = "1h";
-      };
+  services.fail2ban.jails.minecraft = {
+    settings = {
+      filter = "minecraft-whitelist";
+      logpath = "/var/lib/minecraft/data/logs/latest.log";
+      maxretry = "3";
+      bantime = "1h";
     };
   };
   # Custom filter
-  environment.etc."fail2ban/filter.d/minecraft-whitelist.conf".text = lib.generators.toINI {} {
+  environment.etc."fail2ban/filter.d/minecraft.conf".text = lib.generators.toINI {} {
     Definition = {
       failregex = ''
         ^.*com\.mojang\.authlib\.GameProfile\@\.\+\[.*name\=<USER>.*\] \(\/<ADDR>\:[0-9]*\) lost connection\: You are not whitelisted on this server\!.*$
-      '';
-      ignoreregex = "";
-    };
-  };
-  environment.etc."fail2ban/filter.d/minecraft-connection-spam.conf".text = lib.generators.toINI {} {
-    Definition = {
-      failregex = ''
         ^.*\[minecraft/ServerLoginPacketListenerImpl\]\: <USER> \(\/<ADDR>\:[0-9]*\) lost connection\: Disconnected.*$
       '';
       ignoreregex = "";
