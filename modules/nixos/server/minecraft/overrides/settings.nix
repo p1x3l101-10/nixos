0496668cb5c2c5e6d3f841c190750312456ff273
Pixel <scott.blatt.0b10@gmail.com>
2026-04-29 12:20:23 -0500

    change gamerule style

--- modules/nixos/server/minecraft/overrides/settings.nix
+++ modules/nixos/server/minecraft/overrides/settings.nix
@@ -1,14 +1,18 @@
 { packId ? ""
 , jvmArgs ? [ ]
 , userdata
+, gamerules ? { playersSleepingPercentage = 10; }
 }:
 
-{
+let
+  mapAttrsToList = f: attrs: builtins.attrValues (builtins.mapAttrs f attrs);
+in {
   eula = true;
   whitelist = (userdata [ "mcUsername" ] (import ./whitelist.nix)) ++ (import ./whitelist-raw.nix);
-  rcon.startup = [
-    "gamerule playersSleepingPercentage 10"
-  ];
+  rcon.startup = (mapAttrsToList
+    (rule: value: "gamerule ${rule} ${builtins.toString value}")
+    gamerules
+  );
   memory = 8;
   java.args = (
     if (packId == "") then
