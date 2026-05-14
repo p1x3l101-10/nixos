51beba1bc59a646de4633d7d8d9f98b6469ec7f3
Pixel <scott.blatt.0b10@gmail.com>
2026-05-14 09:51:17 -0500

    add fwupd to the esp

--- modules/nixos/base/secureBoot.nix
+++ modules/nixos/base/secureBoot.nix
@@ -1,7 +1,14 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   keydir = "/nix/host/keys/secureboot";
   bootDir = config.boot.loader.efi.efiSysMountPoint;
+  allowedEFI = config.boot.loader.efi.canTouchEfiVariables;
+  fwupd = rec {
+    package = pkgs.fwupd-efi;
+    efi = "${package}/libexec/fwupd/efi/fwupdx64.efi";
+    espDir = "${bootDir}/EFI/fwupd";
+  };
+  sbsign = input: output: "${pkgs.sbsigntool}/bin/sbsign --key ${keydir}/db/db.key --cert ${keydir}/db/db.crt --output ${output} ${input}";
 in
 {
   options.system.useSecureBoot = lib.mkOption {
@@ -9,9 +16,18 @@ in
     type = lib.types.bool;
     default = true;
   };
-  config = lib.mkIf config.system.useSecureBoot {
+  config = if config.system.useSecureBoot then {
     boot = {
-      loader.systemd-boot.enable = lib.mkForce false;
+      loader.systemd-boot = {
+        enable = lib.mkForce false;
+        extraEntries = {
+          "fwupd.conf" = ''
+            title Firmware Updater
+            efi /EFI/fwupd/fwupd.efi
+            sort-key z_fwupd
+          '';
+        };
+      };
       loader.grub.enable = lib.mkForce false;
       lanzaboote = {
         enable = true;
@@ -45,6 +61,48 @@ in
           cp ${keydir}/KEK/KEK.auth ${bootDir}/loader/keys/local
           cp ${keydir}/db/db.auth ${bootDir}/loader/keys/local
         fi
+      '';
+      fwupd-efi.text = lib.mkIf allowedEFI ''
+        ## Sign, and copy the fwupd efi binary to ${bootDir}
+        mkdir -p "${fwupd.espDir}"
+        if ([ -f "${fwupd.espDir}/fwupd.hash" ] && [ $(basename "${fwupd.package}" | cut -d- -f1) != $(cat "${fwupd.espDir}/fwupd.hash")]); then
+          # Package differs, copy the binary
+          echo "Updating fwupd"
+          rm -f "${fwupd.espDir}/fwupd.efi"
+          ${sbsign fwupd.efi "${fwupd.espDir}/fwupd.efi"}
+        fi
+        if [ ! -f "${fwupd.espDir}/fwupd.efi" ]; then
+          # Not installed
+          echo "Installing fwupd to the ESP"
+          ${sbsign fwupd.efi "${fwupd.espDir}/fwupd.efi"}
+        fi
+        basename "${fwupd.package}" | cut -d- -f1 > "${fwupd.espDir}/fwupd.hash"
+      '';
+    };
+  } else {
+    boot.loader.systemd-boot.extraEntries = {
+      "fwupd.conf" = ''
+        title Firmware Updater
+        efi /EFI/fwupd/fwupd.efi
+        sort-key z_fwupd
+      '';
+    };
+    system.activationScripts = {
+      fwupd-efi.text = lib.mkIf allowedEFI ''
+        ## Copy the fwupd efi binary to ${bootDir}
+        mkdir -p "${fwupd.espDir}"
+        if ([ -f "${fwupd.espDir}/fwupd.hash" ] && [ $(basename "${fwupd.package}" | cut -d- -f1) != $(cat "${fwupd.espDir}/fwupd.hash")]); then
+          # Package differs, copy the binary
+          echo "Updating fwupd"
+          rm -f "${fwupd.espDir}/fwupd.efi"
+          cp "${fwupd.efi}" "${fwupd.espDir}/fwupd.efi"
+        fi
+        if [ ! -f "${fwupd.espDir}/fwupd.efi" ]; then
+          # Not installed
+          echo "Installing fwupd to the ESP"
+          cp "${fwupd.efi}" "${fwupd.espDir}/fwupd.efi"
+        fi
+        basename "${fwupd.package}" | cut -d- -f1 > "${fwupd.espDir}/fwupd.hash"
       '';
     };
   };
