a17ce4da258365df2e47a04233d706b205f193de
Pixel <scott.blatt.0b10@gmail.com>
2026-05-29 12:57:42 -0500

    fix iwd again

--- systems/pixels-laptop/hardware-configuration.nix
+++ systems/pixels-laptop/hardware-configuration.nix
@@ -14,8 +14,10 @@
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.framework.enableKmod = true;
   # Start iwd AFTER the wifi device comes online
+  # Adapted from https://wiki.archlinux.org/title/Iwd#Restarting_iwd.service_after_boot
   systemd.services.iwd = {
     wants = [ "sys-devices-pci0000:00-0000:00:02.2-0000:01:00.0-net-wlan0.device" ];
     after = [ "sys-devices-pci0000:00-0000:00:02.2-0000:01:00.0-net-wlan0.device" ];
+    serviceConfig.ExecStartPre = "ip link set wlan0 up";
   };
 }
