0d5bfd751fcf56349167bd34da9907fe1a700692
Pixel <scott.blatt.0b10@gmail.com>
2026-06-03 19:55:08 -0500

    more supporting programs

--- modules/home/desktop/apps/games/elite-dangerous.nix
+++ modules/home/desktop/apps/games/elite-dangerous.nix
@@ -3,7 +3,11 @@
 {
   home.packages = with pkgs; [
     min-ed-launcher
+    edhm-ui
     ed-odyssey-materials-helper
     edmarketconnector
+  ];
+  home.allowedUnfree.packages = [
+    "edhm-ui"
   ];
 }
