cd270f7e02bc6fc8f82d1c530f818f6a0d5958d2
Pixel <scott.blatt.0b10@gmail.com>
2026-07-02 00:05:51 -0500

    SC system settings

--- modules/home/desktop/apps/games/star-citizen.nix
+++ modules/home/desktop/apps/games/star-citizen.nix
@@ -1,14 +1,16 @@
-{ ext, ... }:
+{ ext, osConfig, lib, ... }:
 
 let
   gamePkgs = ext.inputs.nix-gaming.packages."${ext.system}";
 in {
-  home.packages = [
-    (gamePkgs.star-citizen.override {
-      location = "$HOME/.local/share/star-citizen";
-      useUmu = true;
-      enableGlCache = true;
-    })
-  ];
-  home.allowedUnfree.packages = [ "star-citizen" ];
+  config = lib.mkIf (osConfig.networking.hostName == "pixels-pc") {
+    home.packages = [
+      (gamePkgs.star-citizen.override {
+        location = "$HOME/.local/share/star-citizen";
+        useUmu = true;
+        enableGlCache = true;
+      })
+    ];
+    home.allowedUnfree.packages = [ "star-citizen" ];
+  };
 }
--- systems/pixels-pc/default.nix
+++ systems/pixels-pc/default.nix
@@ -12,6 +12,18 @@
   environment.etc.machine-id.text = "c2b9de128d004668baadd6bd861149ad";
   networking.hostId = "c2b9de12";
 
+  # Some games need specific settings, and I am not putting this on something like a laptop
+  ## Star Citizen
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 16777216;
+    "fs.file-max" = 524288;
+  };
+  zramSwap = {
+    enable = true;
+    priority = 6;
+    algorithm = "zstd";
+  };
+
   # Begin host fixes:
   #systemd.oomd.enable = false;
   #systemd.services."systemd-timesyncd".serviceConfig.ProtectKernelModules = "no";
