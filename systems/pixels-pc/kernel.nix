e331d6473e17002fa0045805e1d6ae4e1bc7d74a
Pixel <scott.blatt.0b10@gmail.com>
2026-07-03 02:25:01 -0500

    kernel

--- systems/pixels-pc/kernel.nix
+++ systems/pixels-pc/kernel.nix
@@ -7,13 +7,14 @@ let
     kOpts = with lib; with lib.kernel; (fix (final: {
       yes = mkForce yes;
       no = mkForce no;
-      freeform = mkForce freeform;
+      freeform = x: mkForce (freeform x);
       module = mkForce module;
       unset = mkForce unset;
       y = final.yes;
       n = final.no;
       m = final.module;
       u = final.unset;
+      ff = final.freeform;
     }));
     llvmKernelStdenv = pkgs.stdenvAdapters.overrideInStdenv pkgs.llvmPackages.stdenv (with pkgs; [
       llvm
@@ -61,10 +62,53 @@ let
       # Disable some debugging stuff that causes notable performance hits
       {
         config."no-debugging-symbols" = with kOpts; {
-          DEBUG_KERNEL = n;
+          DEBUG_INFO = u;
+          DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT = n;
+          DEBUG_INFO_REDUCED = u;
+          DEBUG_INFO_BTF = u;
+          # DEBUG_KERNEL = n; # TODO: Find dependancy that still needs this
+          DYNAMIC_DEBUG = n;
           PROFILING = n;
-          AUDIT = n;
-          AUDITSYSCALL = n;
+          CRASH_DUMP = n;
+          SUNRPC_DEBUG = n;
+          SECURITY_IPE = n;
+          IPE_PROP_FS_VERITY = u;
+          IPE_PROP_FS_VERITY_BUILTIN_SIG = u;
+          BPF_LSM = n;
+          SCHEDSTATS = n;
+          IRQ_TIME_ACCOUNTING = n;
+          PARAVIRT_TIME_ACCOUNTINT = u;
+          NETCONSOLE = n;
+          NETCONSOLE_DYNAMIC = u;
+          PRINTK_INDEX = n;
+        };
+      }
+      # Hijack Archlinux's qr code decompressor
+      {
+        config."blatant-qr-theft" = with kOpts; {
+          DRM_PANIC = y;
+          DRM_PANIC_SCREEN = ff "qr_code";
+          DRM_PANIC_SCREEN_QR_CODE = y;
+          DRM_PANIC_SCREEN_QR_CODE_URL = ff "https://panic.archlinux.org/panic_report#";
+        };
+      }
+      # Make my panic look not BSOD
+      {
+        config."no-bsod" = with kOpts; {
+          DRM_PANIC_FOREGROUND_COLOR = ff "0xffffff";
+          DRM_PANIC_BACKGROUND_COLOR = ff "0xff0000";
+        };
+      }
+      # Fix some dependancy issues
+      {
+        config."fix-dependancy-issues" = with kOpts; {
+          # Unknown
+          MITIGATION_SLS = u;
+          NOVA_CORE = u;
+          DRM_NOVA = u;
+          NET_SCH_BPF = u;
+          PROC_VMCORE = u;
+          SCHED_CLASS_EXT = u;
         };
       }
     ]
@@ -74,4 +118,6 @@ in {
     kernelPatches = cfg.patches;
     kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (kernel.kernel.override cfg.overrides));
   };
+  # Disable services that are borked by kconfig
+  security.audit.enable = false;
 }
