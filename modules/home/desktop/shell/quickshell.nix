a6d555ffa5eee2cc511515052a7389934fe400f8
Pixel <scott.blatt.0b10@gmail.com>
2026-07-07 08:26:40 -0500

    change quickshell config

--- modules/home/desktop/shell/quickshell.nix
+++ modules/home/desktop/shell/quickshell.nix
@@ -1,8 +1,10 @@
-{ pkgs, osConfig, ... }:
+{ pkgs, ... }:
 
 {
   programs.quickshell = {
     enable = true;
+    configs.main = ./config;
+    activeConfig = "main";
     systemd = {
       enable = true;
       target = "hyprland-session.target";
@@ -12,7 +14,11 @@
     quickshell
     material-symbols
   ];
-  systemd.user.tmpfiles.rules = [
-    "L /home/pixel/.config/quickshell - - - - ${osConfig.environment.etc."nixos".source}/modules/home/desktop/shell/config"
+  systemd.user.services.quickshell.Service.Environment = [
+    "QS_APP_ID=org.exsmachina.shell"
+    "QS_DISABLE_FILE_WATCHER=1"
+    "QS_DISABLE_CRASH_HANDLER=1"
+    "QS_NO_RELOAD_POPUP=1"
+    "QS_DROP_EXPENSIVE_FONTS=1"
   ];
 }
