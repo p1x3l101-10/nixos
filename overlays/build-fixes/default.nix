2d1effa1f09e5f1547c2b07ddb3faaa658265bc6
Pixel <scott.blatt.0b10@gmail.com>
2026-07-19 00:39:38 -0500

    disable check

--- overlays/build-fixes/default.nix
+++ overlays/build-fixes/default.nix
@@ -2,4 +2,11 @@ final: prev: {
   assimp = prev.assimp.overrideAttrs (oldAttrs: {
     doCheck = false;
   });
+  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+    (pFinal: pPrev: {
+      patool = pPrev.patool.overrideAttrs (old: {
+        doCheck = false;
+      });
+    })
+  ];
 }
