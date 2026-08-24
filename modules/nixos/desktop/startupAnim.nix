cdbf1a8d1d8d37d66d0fbd82dd4e65d0ff1747b0
Pixel <scott.blatt.0b10@gmail.com>
2026-08-24 12:53:00 -0500

    use BGRT instead of stylix for boot animation

--- modules/nixos/desktop/startupAnim.nix
+++ modules/nixos/desktop/startupAnim.nix
@@ -4,6 +4,7 @@
   boot = {
     plymouth = {
       enable = true;
+      theme = "bgrt"; # NOTE: Stylix conflicts with this; if enabling stylix again, be sure to remove this option
     };
     consoleLogLevel = 0;
     kernelParams = [
@@ -12,4 +13,6 @@
     ];
     loader.timeout = 0;
   };
+  # Disable stylix because we want our own theme
+  stylix.targets.plymouth.enable = false;
 }
