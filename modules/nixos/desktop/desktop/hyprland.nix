7ded0d69557be9e0c3472b34bcf8aea9e0dce9c9
Pixel <scott.blatt.0b10@gmail.com>
2026-08-27 11:31:57 -0500

    Revert flake updates
    This reverts commit 52ba8b7b7f0564aa9b4aa97e17a043517a3f4920.
    
    Revert "fixes"
    
    This reverts commit 9b7f93c85c79c0d9ae87c9537703f1514de28b66.
    
    Revert "fix pin"
    
    This reverts commit 4e2125e3520773899021840be9b19295fcad13b0.
    
    Revert "force a working version of hyprland"
    
    This reverts commit 4d33a7db1f1e67794e25826cce63a63b62640fc0.
    
    Revert "use legacy uwsm"
    
    This reverts commit 6d72f83b69def1e863074f55e52901fda7ad4b24.
    
    Revert "use legacy uwsm"
    
    This reverts commit 6d72f83b69def1e863074f55e52901fda7ad4b24.
    
    Revert "flake.lock: Update"
    
    This reverts commit 89e25c603a44e51010a95b59bdb6d376966a76ca.

--- modules/nixos/desktop/desktop/hyprland.nix
+++ modules/nixos/desktop/desktop/hyprland.nix
@@ -1,19 +1,20 @@
-{ lib, ext, ... }:
+{ pkgs, lib, ext, ... }:
 
 let
-  hyprPkgs = ext.hyprPin.pkgs;
+  hyprLib = import ../../../home/desktop/hyprland/support/hypr-lib.nix { inherit lib ext; };
+  hyprGlobals = import ../../../home/desktop/hyprland/support/hypr-globals.nix { inherit pkgs lib ext hyprLib; };
+  inherit (hyprGlobals) clockFormat;
 in
 
 lib.fix (self: {
   programs.hyprland = {
     enable = true;
-    package = hyprPkgs.hyprland;
     withUWSM = true;
     xwayland.enable = true;
   };
   xdg.portal = {
     enable = true;
-    #extraPortals = with hyprPkgs; [ xdg-desktop-portal-hyprland ];
+    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
   };
   services.udisks2.enable = true;
   programs.uwsm.enable = true;
