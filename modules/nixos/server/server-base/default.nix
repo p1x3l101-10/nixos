c9a8f26f2d305f37fb30a9fd4bd32516484ca213
Pixel <scott.blatt.0b10@gmail.com>
2026-08-30 15:47:37 -0500

    im sick and tired of doing this, so I wrote a wrapper
    As all good sysadmins do, Im automating my job away until I simply stare
    at the computer until it breaks

--- modules/nixos/server/server-base/default.nix
+++ modules/nixos/server/server-base/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, config, ... }:
 
 {
   imports = [
@@ -9,5 +9,25 @@
     ./speed.nix
     ./ssh.nix
     ./user.nix
+  ];
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "repo-update";
+      runtimeInputs = with config; [
+        programs.git.package
+        programs.nixos-cli.package
+      ];
+      text = ''
+        if [[ $UID != 1000 ]]; then
+          systemd-run --uid=1000 "$(realpath $0)"
+        else
+          git -C /etc/nixos reset --hard
+          git -C /etc/nixos pull
+        fi
+        if [[ $1 == "switch" ]]; then
+          nixos apply -y
+        fi
+      '';
+    })
   ];
 }
