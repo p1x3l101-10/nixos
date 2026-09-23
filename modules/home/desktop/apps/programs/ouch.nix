126c6897c62a61d01482181361db7d769f6c65fb
Pixel <scott.blatt.0b10@gmail.com>
2026-09-23 12:04:31 -0500

    license

--- modules/home/desktop/apps/programs/ouch.nix
+++ modules/home/desktop/apps/programs/ouch.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ ouch ouch-rar ];
+  home.packages = with pkgs; [
+    #ouch
+    ouch-rar
+  ];
+  home.allowedUnfree.packages = [
+    "ouch" # the rar component is unfree, everything else is free
+  ];
 }
